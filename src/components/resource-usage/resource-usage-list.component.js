9a3b52e980ac0dad78537b3bae6b64d144f63792
Christian <c.pfarher@gmail.com>
2020-08-17 15:17:01 -0300

    reindent an api integration

--- src/components/resource-usage/resource-usage-list.component.js
+++ src/components/resource-usage/resource-usage-list.component.js
@@ -1,14 +1,18 @@
 import * as React from "react";
-import { Icon, Table, Progress } from "semantic-ui-react";
+//import { observer } from "mobx-react";
+import { Icon, Table, Progress, Label, Popup } from "semantic-ui-react";
 
 import AssetIdComponent from "../utils/asset-id.component";
 import ShowDeviceIcon from "../utils/show-device-icon.component";
 import ShowDeviceState from "./show-device-state.component";
 import ShowMessagesSummary from "./show-message-summary.component";
 import EmptyComponent from "../utils/empty.component";
+import WifiIndicator, { DBMToSignalStrength } from "react-wifi-indicator";
+import NumberFormat from "react-number-format";
+import moment from "moment";
 
 const ResourceUsageList = (props) => {
-  return (
+  return (    
     <Table striped className="animated fadeIn" basic="very" compact="very">
       <Table.Header>
         <Table.Row>
@@ -17,7 +21,7 @@ const ResourceUsageList = (props) => {
           </Table.HeaderCell>
           <Table.HeaderCell collapsing>ID</Table.HeaderCell>
           <Table.HeaderCell>NAME</Table.HeaderCell>
-          <Table.HeaderCell>STATE</Table.HeaderCell>
+          <Table.HeaderCell>LAST MESSAGE</Table.HeaderCell>
           <Table.HeaderCell>
             MESSAGES <i>(R/S/L)</i>
           </Table.HeaderCell>
@@ -46,6 +50,7 @@ const ResourceUsageList = (props) => {
             props.list.data.map((item, index) => {
               return (
                 <Table.Row key={index} style={{ cursor: "pointer" }}>
+                  {console.log(item)}
                   <Table.Cell style={{ textAlign: "center" }}>
                     <ShowDeviceIcon
                       type={
@@ -62,32 +67,71 @@ const ResourceUsageList = (props) => {
                     <AssetIdComponent type={item.type} id={item.hex_id} />
                   </Table.Cell>
                   <Table.Cell>
-                    {ShowDeviceState(item.state)}
+                    {ShowDeviceState(item.connected)}
                     {item.name}
                   </Table.Cell>
-                  <Table.Cell>??</Table.Cell>
                   <Table.Cell>
-                    {ShowMessagesSummary(
-                      item.received,
-                      item.sended,
-                      item.lost,
-                      item.received_p,
-                      item.sended_p,
-                      item.lost_p
-                    )}
+                    <Popup
+                      trigger={
+                        <span>{moment.unix(item.last_activity).fromNow()}</span>
+                      }
+                      position="bottom left"
+                    >
+                      <Popup.Header>Last message received</Popup.Header>
+                      <Popup.Content>
+                        {moment
+                          .unix(item.last_activity)
+                          .format("dddd, MMMM Do, YYYY h:mm:ss A")}
+                      </Popup.Content>
+                    </Popup>
                   </Table.Cell>
-                  <Table.Cell>{item.package_frequency}</Table.Cell>
                   <Table.Cell>
-                    {item.type == "device" && (
-                      <Progress
+                    <ShowMessagesSummary
+                      packets_down={item.packets_down}
+                      packets_lost={item.packets_lost}
+                      packets_up={item.packets_up}
+                    ></ShowMessagesSummary>
+                  </Table.Cell>
+                  <Table.Cell
+                    title="frequency between packages"
+                    className="aligned pull-right"
+                  >
+                    <NumberFormat
+                      value={item.activity_freq}
+                      displayType={"text"}
+                      prefix={"every "}
+                      suffix={" s."}
+                      decimalScale="1"
+                    />
+                  </Table.Cell>
+                  <Table.Cell style={{ padding: "0px" }}>
+                    <div class="aligned pull-right">
+                      {item.type.toLowerCase().trim() == "device" && (
+                        <WifiIndicator
+                          strength={DBMToSignalStrength(item.max_rssi)}
+                          style={{
+                            height: 20,
+                            verticalAlign: "bottom",
+                          }}
+                        />
+                        /*<Progress
                         size="medium"
                         color="green"
                         value={item.signal_strength}
                         total={100}
                         active
                         progress="percent"
-                      ></Progress>
-                    )}
+                      ></Progress>*/
+                      )}
+                      <strong style={{ marginLeft: "5px" }}>
+                        <NumberFormat
+                          value={item.max_rssi}
+                          displayType={"text"}
+                          suffix={" dBm"}
+                          decimalScale="1"
+                        />
+                      </strong>
+                    </div>
                   </Table.Cell>
                 </Table.Row>
               );
--- src/components/resource-usage/resource-usage.component.js
+++ src/components/resource-usage/resource-usage.component.js
@@ -1,5 +1,5 @@
-import React, {useState } from "react";
-import { observer, inject } from "mobx-react";
+import React, {useState, useEffect } from "react";
+import { inject, observer } from "mobx-react";
 
 import {
   Segment,
@@ -31,7 +31,7 @@ const clearFilters = () => {
     isLoading: true,
   });
 
-  this.loadAssetsAndCounts();
+  //this.loadAssetsAndCounts();
 };
 
 const handlePaginationChange = (e, { activePage }) => {
@@ -57,207 +57,210 @@ const handlePaginationChange = (e, { activePage }) => {
   };
 
 //******************************************************* */
-// const { activePage, pageSize, criteria } = this.state;
-const loadAssets = (props) => {
-  const assetsPromise = props.resourceUssageStore.getAssets(
-    {},
-    //{ page: activePage, size: pageSize },
-    {}
-    //criteria
-  );
-  Promise.all([assetsPromise]).then(
-        (responses) => {
-          console.log(responses[0].data);
-          //this.setState({data: responses[0].data});
-      }
-  );
-}
 
 const ResourceUsageComponent = (props) => {
-  const [showFilters, setShowFilters] = React.useState(true);
-  const [criteria, setCriteria] = React.useState({
-    type: null,
-  });
-  
-  loadAssets(props);
+        const [showFilters, setShowFilters] = React.useState(true);
+        const [criteria, setCriteria] = React.useState({
+          type: null,
+        });
 
-  const [loading, setLoading] = React.useState(false);
-  const [currentPage, setCurrentPage] = React.useState(1);
-  const [perPage, setPerPage] = React.useState(20);
-  
-  const [list, setList] = React.useState({
-    activePage: 1,
-    pagesCount: 1,
-    isLoading: false,
-    count: 2,
-    data: [
-      {
-        hex_id: "FFFFFFFFFF",
-        name: "device name",
-        type: "device",
-        state: "connected",
-        received: 123,
-        received_p: 10.5,
-        sended: 456,
-        sended_p: 50.4,
-        lost: 789,
-        lost_p: 39.1,
-        signal_strength: 50,
-      },
-      {
-        hex_id: "0000000000",
-        name: "gateway name",
-        type: "gateway",
-        state: "disconnected",
-        received: 123,
-        received_p: 10.5,
-        sended: 456,
-        sended_p: 50.4,
-        lost: 789,
-        lost_p: 39.1,
-        signal_strength: 20,
-      },
-    ],
-  });
+        const [loading, setLoading] = React.useState(false);
+        const [currentPage, setCurrentPage] = React.useState(1);
+        const [perPage, setPerPage] = React.useState(20);
 
-  return (
-    <div className="app-body-container-view">      
-      <div className="animated fadeIn animation-view">
-        <div className="view-header">
-          <h1 className="mb0">RESOURCES USAGE</h1>
-          <div className="view-header-actions">
-            {!showFilters && (
-              <div onClick={() => setShowFilters(true)}>
-                <i className="fas fa-eye" />
-                <span>SHOW SEARCH AND CHARTS</span>
-              </div>
-            )}
-            {showFilters && (
-              <div
-                onClick={() => setShowFilters(false)}
-                style={{ color: "gray" }}
-              >
-                <i className="fas fa-eye-slash" />
-                <span>HIDE SEARCH AND CHARTS</span>
-              </div>
-            )}
-          </div>
-        </div>
-        {showFilters && (
-          <Segment>
-            <Grid className="animated fadeIn">
-              <Grid.Row
-                id="visualization-container"
-                className="data-container pl pr"
-              >
-                <Grid.Column
-                  className="data-container-box pl0 pr0"
-                  mobile={16}
-                  tablet={8}
-                  computer={4}
-                >
-                  <div className="box-data">
-                    <h5 style={{ color: "gray" }}>WORK IN PROGRESS</h5>
-                    <i
-                      style={{ color: "gray", align: "middle" }}
-                      className="fas fa-exclamation fa-4x"
-                    ></i>
-                  </div>
-                </Grid.Column>
+        const [list, setList] = React.useState({
+          activePage: 1,
+          pagesCount: 1,
+          isLoading: false,
+          count: 2,
+          data: [
+            {
+              hex_id: "FFFFFFFFFF",
+              name: "device name",
+              type: "device",
+              connected: true,
+              max_rssi: 50,
+              id: 1,
+              data_collector: "Chirpstack.io",
+              app_name: null,
+              packets_down: {total: 3306, per_minute: 4.894177805965373, per_hour: 293.65066835792237, per_day: 7047.616040590136, percentage: 56.59020883259158},
+              packets_lost: {total: 3306, per_minute: 4.894177805965373, per_hour: 293.65066835792237, per_day: 7047.616040590136, percentage: 56.59020883259158},
+              packets_up: {total: 2536, per_minute: 3.754275534158556, per_hour: 225.25653204951337, per_day: 5406.15676918832, percentage: 43.40979116740842}
+            },
+            {
+              hex_id: "0000000000",
+              name: "gateway name",
+              type: "gateway",
+              connected: true,
+              max_rssi: 50,
+              id: 1,
+              data_collector: "Chirpstack.io",
+              app_name: null,
+              packets_down: {total: 3306, per_minute: 4.894177805965373, per_hour: 293.65066835792237, per_day: 7047.616040590136, percentage: 56.59020883259158},
+              packets_lost: {total: 3306, per_minute: 4.894177805965373, per_hour: 293.65066835792237, per_day: 7047.616040590136, percentage: 56.59020883259158},
+              packets_up: {total: 2536, per_minute: 3.754275534158556, per_hour: 225.25653204951337, per_day: 5406.15676918832, percentage: 43.40979116740842}
+            },
+          ],
+        });
 
-                <Grid.Column
-                  className="data-container-box pl0 pr0"
-                  mobile={16}
-                  tablet={8}
-                  computer={4}
-                >
-                  <div className="box-data">
-                    <h5 style={{ color: "gray" }}>WORK IN PROGRESS</h5>
-                    <i
-                      style={{ color: "gray", align: "middle" }}
-                      className="fas fa-exclamation fa-4x"
-                    ></i>
-                  </div>
-                </Grid.Column>
+        useEffect(() => {
+          const assetsPromise = props.resourceUssageStore.getAssets(
+            {}, //page: activePage, size: pageSize
+            criteria
+          );
+         Promise.all([assetsPromise]).then((response) => {
+           console.log(response[0].data.assets);
+           setList((oldList) => {
+             response[0].data.assets.map(e => { // preprocess data!
+               e.packets_down = {...{total:'-', per_minute:'-', per_hour:'-',per_day:'-',percentage:'-'}, ...e.packets_down}
+               e.packets_up = {...{total:'-', per_minute:'-', per_hour:'-',per_day:'-',percentage:'-'}, ...e.packets_up}
+               e.packets_lost = {...{total:'-', per_minute:'-', per_hour:'-',per_day:'-',percentage:'-'}, ...e.packets_lost}
+             })
+             return { ...oldList, data: response[0].data.assets };
+           });
+         });
+        }, 
+        []); // Solo se vuelve a ejecutar si cambia lo de []
 
-                <Grid.Column
-                  className="data-container-box pl0 pr0"
-                  mobile={16}
-                  tablet={8}
-                  computer={4}
-                >
-                  <div className="box-data">
-                    <h5 style={{ color: "gray" }}>WORK IN PROGRESS</h5>
-                    <i
-                      style={{ color: "gray", aling: "middle" }}
-                      className="fas fa-exclamation fa-4x"
-                    ></i>
-                  </div>
-                </Grid.Column>
-
-                <Grid.Column
-                  className="data-container-box pl0 pr0"
-                  mobile={16}
-                  tablet={8}
-                  computer={4}
-                >
-                  <div className="box-data">
-                    <h5 style={{ color: "gray" }}>WORK IN PROGRESS</h5>
-                    <i
-                      style={{ color: "gray", aling: "middle" }}
-                      className="fas fa-exclamation fa-4x"
-                    ></i>
-                  </div>
-                </Grid.Column>
-              </Grid.Row>
-            </Grid>
-          </Segment>
-        )}
-        <div className="view-body">
-          <div className="table-container">
-            <div className="table-container-box">
-              <Segment>
-                {showFilters && (
-                  <div>
-                    <label style={{ fontWeight: "bolder" }}>Filters: </label>
-                    <span
-                      className="range-select"
-                      onClick={() => clearFilters()}
+        return (
+          <div className="app-body-container-view">
+            <div className="animated fadeIn animation-view">
+              <div className="view-header">
+                <h1 className="mb0">RESOURCES USAGE</h1>                
+                <div className="view-header-actions">
+                  {!showFilters && (
+                    <div onClick={() => setShowFilters(true)}>
+                      <i className="fas fa-eye" />
+                      <span>SHOW SEARCH AND CHARTS</span>
+                    </div>
+                  )}
+                  {showFilters && (
+                    <div
+                      onClick={() => setShowFilters(false)}
+                      style={{ color: "gray" }}
                     >
-                      Clear
-                    </span>
-                  </div>
-                )}
-                {!list.isLoading && (
-                  <ResourceUsageList
-                    list={list}
-                    criteria={criteria}
-                  ></ResourceUsageList>
-                )}
+                      <i className="fas fa-eye-slash" />
+                      <span>HIDE SEARCH AND CHARTS</span>
+                    </div>
+                  )}
+                </div>
+              </div>
+              {showFilters && (
+                <Segment>
+                  <Grid className="animated fadeIn">
+                    <Grid.Row
+                      id="visualization-container"
+                      className="data-container pl pr"
+                    >
+                      <Grid.Column
+                        className="data-container-box pl0 pr0"
+                        mobile={16}
+                        tablet={8}
+                        computer={4}
+                      >
+                        <div className="box-data">
+                          <h5 style={{ color: "gray" }}>WORK IN PROGRESS</h5>
+                          <i
+                            style={{ color: "gray", align: "middle" }}
+                            className="fas fa-exclamation fa-4x"
+                          ></i>
+                        </div>
+                      </Grid.Column>
 
-                {list.isLoading && (
-                  <LoaderComponent
-                    loadingMessage="Loading resource usage..."
-                    style={{ marginBottom: 20 }}
-                  />
-                )}
-                {!list.isLoading && list.pagesCount > 1 && (
-                  <Grid className="segment centered">
-                    <Pagination
-                      className=""
-                      activePage={list.activePage}
-                      onPageChange={handlePaginationChange}
-                      totalPages={list.pagesCount}
-                    />
+                      <Grid.Column
+                        className="data-container-box pl0 pr0"
+                        mobile={16}
+                        tablet={8}
+                        computer={4}
+                      >
+                        <div className="box-data">
+                          <h5 style={{ color: "gray" }}>WORK IN PROGRESS</h5>
+                          <i
+                            style={{ color: "gray", align: "middle" }}
+                            className="fas fa-exclamation fa-4x"
+                          ></i>
+                        </div>
+                      </Grid.Column>
+
+                      <Grid.Column
+                        className="data-container-box pl0 pr0"
+                        mobile={16}
+                        tablet={8}
+                        computer={4}
+                      >
+                        <div className="box-data">
+                          <h5 style={{ color: "gray" }}>WORK IN PROGRESS</h5>
+                          <i
+                            style={{ color: "gray", align: "middle" }}
+                            className="fas fa-exclamation fa-4x"
+                          ></i>
+                        </div>
+                      </Grid.Column>
+
+                      <Grid.Column
+                        className="data-container-box pl0 pr0"
+                        mobile={16}
+                        tablet={8}
+                        computer={4}
+                      >
+                        <div className="box-data">
+                          <h5 style={{ color: "gray" }}>WORK IN PROGRESS</h5>
+                          <i
+                            style={{ color: "gray", align: "middle" }}
+                            className="fas fa-exclamation fa-4x"
+                          ></i>
+                        </div>
+                      </Grid.Column>
+                    </Grid.Row>
                   </Grid>
-                )}
-              </Segment>
+                </Segment>
+              )}
+              <div className="view-body">
+                <div className="table-container">
+                  <div className="table-container-box">
+                    <Segment>
+                      {showFilters && (
+                        <div>
+                          <label style={{ fontWeight: "bolder" }}>
+                            Filters:{" "}
+                          </label>
+                          <span
+                            className="range-select"
+                            onClick={() => clearFilters()}
+                          >
+                            Clear
+                          </span>
+                        </div>
+                      )}
+                      {!list.isLoading && (
+                        <ResourceUsageList
+                          list={list}
+                          criteria={criteria}
+                        ></ResourceUsageList>
+                      )}
+
+                      {list.isLoading && (
+                        <LoaderComponent
+                          loadingMessage="Loading resource usage..."
+                          style={{ marginBottom: 20 }}
+                        />
+                      )}
+                      {!list.isLoading && list.pagesCount > 1 && (
+                        <Grid className="segment centered">
+                          <Pagination
+                            className=""
+                            activePage={list.activePage}
+                            onPageChange={handlePaginationChange}
+                            totalPages={list.pagesCount}
+                          />
+                        </Grid>
+                      )}
+                    </Segment>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
-    </div>
-  );
-};
-
+        );
+      };
 export default inject("resourceUssageStore")(ResourceUsageComponent);
