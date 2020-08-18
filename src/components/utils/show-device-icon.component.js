4f315cfc6baacfa28fa1866360fa22511eac86ca
Christian <c.pfarher@gmail.com>
2020-08-18 19:45:05 -0300

    integrate handler click event

--- src/components/utils/show-device-icon.component.js
+++ src/components/utils/show-device-icon.component.js
@@ -8,23 +8,30 @@
 import React from "react";
 import "./show-device-icon.component.css";
 
+const getHandlerClick = (func) => {
+  if (typeof func === "function") {
+    return func
+  } else {
+    return undefined;
+  }
+}
+
 function ShowDeviceIcon(props) {
   
   const type = props && props.type && props.type.toLowerCase().trim();
-
   const deviceTitle = "device"
   const gatewayTitle = "gateway";
   const unknown = "unknown";
 
   if (type === deviceTitle) {
-    return <i className="fas fa-microchip" title={deviceTitle} />;
+    return <i onClick={getHandlerClick(props.clickHandler)} className="fas fa-microchip" title={deviceTitle} />;
   } else if (type === gatewayTitle) {
-    return <i className="fas fa-broadcast-tower" title={gatewayTitle} />;
+    return <i onClick={getHandlerClick(props.clickHandler)} className="fas fa-broadcast-tower" title={gatewayTitle} />;
   } else if (type === unknown) {
-    return <i className="fas fa-question" title={gatewayTitle} />;
+    return <i onClick={getHandlerClick(props.clickHandler)} className="fas fa-question" title={gatewayTitle} />;
   } else {
     return (
-      <span class="pointer">
+      <span onClick={getHandlerClick(props.clickHandler)} className="pointer">
         <i className="fas fa-broadcast-tower" title={gatewayTitle} />
         /
         <i className="fas fa-microchip" title={deviceTitle} />
