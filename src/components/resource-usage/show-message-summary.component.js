515ea855f463ec2f6e4b3ba3cf167bfc62031e4a
Christian <c.pfarher@gmail.com>
2020-08-18 09:14:47 -0300

    add pie bar template

--- src/components/resource-usage/show-message-summary.component.js
+++ src/components/resource-usage/show-message-summary.component.js
@@ -6,6 +6,8 @@ const ShowMessagesSummary = (props) => {
   return (
     <span>
       <Popup
+        position="left center"
+        flowing
         trigger={
           <span>
             <Icon
@@ -47,73 +49,79 @@ const ShowMessagesSummary = (props) => {
       >
         <Popup.Header>Messages on the last 24 hours</Popup.Header>
         <Popup.Content>
-          <div>
-            <Icon
-              color="green"
-              name="arrow alternate circle down"
-              type="icon"
-            />
-            Received:
-            <strong>
-              <NumberFormat
-                value={props.packets_down.total}
-                displayType={"text"}
-              />{" "}
-              (
-              <NumberFormat
-                value={props.packets_down.percentage}
-                displayType={"text"}
-                suffix={"%"}
-                decimalScale="1"
+          <div class="eight wide column">
+            <div>
+              <Icon
+                color="green"
+                name="arrow alternate circle down"
+                type="icon"
               />
-              )
-            </strong>
-          </div>
-          <div>
-            <Icon color="orange" name="arrow alternate circle up" type="icon" />
-            Sent:
-            <strong>
-              <NumberFormat
-                value={props.packets_up.total}
-                displayType={"text"}
-              />{" "}
-              (
-              <NumberFormat
-                value={props.packets_up.percentage}
-                displayType={"text"}
-                suffix={"%"}
-                decimalScale="1"
+              Received:
+              <strong>
+                <NumberFormat
+                  value={props.packets_down.total}
+                  displayType={"text"}
+                />{" "}
+                (
+                <NumberFormat
+                  value={props.packets_down.percentage}
+                  displayType={"text"}
+                  suffix={"%"}
+                  decimalScale="1"
+                />
+                )
+              </strong>
+            </div>
+            <div>
+              <Icon
+                color="orange"
+                name="arrow alternate circle up"
+                type="icon"
               />
-              )
-            </strong>
+              Sent:
+              <strong>
+                <NumberFormat
+                  value={props.packets_up.total}
+                  displayType={"text"}
+                />{" "}
+                (
+                <NumberFormat
+                  value={props.packets_up.percentage}
+                  displayType={"text"}
+                  suffix={"%"}
+                  decimalScale="1"
+                />
+                )
+              </strong>
+            </div>
+            <div>
+              <Icon color="grey" name="exclamation triangle" type="icon" />
+              Lost:
+              <strong>
+                <NumberFormat
+                  value={props.packets_lost.total}
+                  displayType={"text"}
+                />
+                (
+                <NumberFormat
+                  value={props.packets_lost.percentage}
+                  displayType={"text"}
+                  suffix={"%"}
+                  decimalScale="1"
+                />
+                )
+              </strong>
+            </div>
           </div>
-          <div>
-            <Icon color="grey" name="exclamation triangle" type="icon" />
-            Lost:
-            <strong>
-              <NumberFormat
-                value={props.packets_lost.total}
-                displayType={"text"}
-              />
-              (
-              <NumberFormat
-                value={props.packets_lost.percentage}
-                displayType={"text"}
-                suffix={"%"}
-                decimalScale="1"
-              />
-              )
-            </strong>
+          <div class="eight wide column">
+            <div className="empty-container">
+              <h1 className="empty-message">
+                <i className="fas fa-info-circle" />
+                <br />
+                pie chart ?
+              </h1>
+            </div>
           </div>
-
-          <div className="empty-container">
-            <h1 className="empty-message">
-              <i className="fas fa-info-circle" />
-              <br />
-              pie chart in progress...
-            </h1>
-          </div>
-
         </Popup.Content>
       </Popup>
     </span>
