36924028d9b2f19a9094da815a4e4bef8033f510
Christian <c.pfarher@gmail.com>
2020-08-19 16:41:06 -0300

    show number instead of percentage and add pie graph

--- src/components/resource-usage/show-message-summary.component.js
+++ src/components/resource-usage/show-message-summary.component.js
@@ -1,8 +1,66 @@
 import * as React from "react";
-import { Icon, Popup } from "semantic-ui-react";
+import { Icon, Popup, Grid, Table } from "semantic-ui-react";
 import NumberFormat from "react-number-format";
 
+import Chart from "react-apexcharts";
 const ShowMessagesSummary = (props) => {
+  const data = {
+    options: {
+      chart: {
+        id: "basic-bar",
+        animations: {
+          enabled: false,
+        },
+      },
+      dataLabels: {
+        enabled: false,
+        textAnchor: "start",
+        offsetX: 200,
+        offsetY: 200,
+        style: {
+          fontSize: "14px",
+          fontFamily: "Helvetica, Arial, sans-serif",
+          fontWeight: "bold",
+          colors: ["#fff"],
+        },
+        dropShadow: {
+          enabled: true,
+          left: 2,
+          top: 2,
+          opacity: 0.5,
+        },
+        formatter: function(val) {
+          return val.toFixed(1);
+        },
+      },
+      legend: {
+        show: false,
+        position: "bottom",
+        fontSize: "12px",
+        fontFamily: "Helvetica, Arial",
+        fontWeight: 400,
+      },
+      labels: ["Sent", "Received", "Lost"],
+      colors: ["#f2711c", "#21ba45", "#767676"],
+      fill: {
+        type: "gradient",
+      },
+    },
+    series: [
+      props.packets_up.percentage == "-" ||
+      props.packets_up.percentage == null
+        ? 0
+        : props.packets_up.percentage,
+      props.packets_down.percentage == "-" ||
+      props.packets_down.percentage == null
+        ? 0
+        : props.packets_down.percentage,
+      props.packets_lost.percentage == "-" ||
+      props.packets_lost.percentage == null
+        ? 0
+        : props.packets_lost.percentage,
+    ],
+  };
   return (
     <span>
       <Popup
@@ -10,124 +68,166 @@ const ShowMessagesSummary = (props) => {
         flowing
         trigger={
           <span>
-            <Icon
-              color="green"
-              name="arrow alternate circle down"
-              type="icon"
-            />
-            <strong>
-              <NumberFormat
-                value={props.packets_down.percentage}
-                displayType={"text"}
-                suffix={"%"}
-                decimalScale="1"
-              />
-            </strong>{" "}
-            /
-            <Icon color="orange" name="arrow alternate circle up" type="icon" />
-            <strong>
-              <NumberFormat
-                value={props.packets_up.percentage}
-                displayType={"text"}
-                suffix={"%"}
-                decimalScale="1"
-              />
-            </strong>
-            {props.type == "device" && (
-              <React.Fragment>
-                {" "}
-                /
-                <Icon color="grey" name="exclamation triangle" type="icon" />
-                <strong>
-                  <NumberFormat
-                    value={props.packets_lost.percentage}
-                    displayType={"text"}
-                    suffix={"%"}
-                    decimalScale="1"
+            <Grid divided>
+              <Grid.Row>
+                <Grid.Column width={4}>
+                  <Icon
+                    color="green"
+                    name="arrow alternate circle down"
+                    type="icon"
                   />
-                </strong>
-              </React.Fragment>
-            )}
+                  <strong>
+                    <NumberFormat
+                      value={props.packets_down.total}
+                      displayType={"text"}
+                    />
+                  </strong>
+                </Grid.Column>
+                <Grid.Column width={4}>
+                  <Icon
+                    color="orange"
+                    name="arrow alternate circle up"
+                    type="icon"
+                  />
+                  <strong>
+                    <NumberFormat
+                      value={props.packets_up.total}
+                      displayType={"text"}
+                    />
+                  </strong>
+                </Grid.Column>
+                <Grid.Column width={4}>
+                  {props.type == "device" && (
+                    <React.Fragment>
+                      <Icon
+                        color="grey"
+                        name="exclamation triangle"
+                        type="icon"
+                      />
+                      <strong>
+                        <NumberFormat
+                          value={props.packets_lost.total}
+                          displayType={"text"}
+                        />
+                      </strong>
+                    </React.Fragment>
+                  )}
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
           </span>
         }
         position="bottom left"
       >
         <Popup.Header>Messages on the last 24 hours</Popup.Header>
         <Popup.Content>
-          <div className="eight wide column">
-            <div>
-              <Icon
-                color="green"
-                name="arrow alternate circle down"
-                type="icon"
-              />
-              Received:
-              <strong>
-                <NumberFormat
-                  value={props.packets_down.total}
-                  displayType={"text"}
-                />{" "}
-                (
-                <NumberFormat
-                  value={props.packets_down.percentage}
-                  displayType={"text"}
-                  suffix={"%"}
-                  decimalScale="1"
-                />
-                )
-              </strong>
-            </div>
-            <div>
-              <Icon
-                color="orange"
-                name="arrow alternate circle up"
-                type="icon"
-              />
-              Sent:
-              <strong>
-                <NumberFormat
-                  value={props.packets_up.total}
-                  displayType={"text"}
-                />{" "}
-                (
-                <NumberFormat
-                  value={props.packets_up.percentage}
-                  displayType={"text"}
-                  suffix={"%"}
-                  decimalScale="1"
-                />
-                )
-              </strong>
-            </div>
-            {props.type == "device" && (
-            <div>
-              <Icon color="grey" name="exclamation triangle" type="icon" />
-              Lost:
-              <strong>
-                <NumberFormat
-                  value={props.packets_lost.total}
-                  displayType={"text"}
-                />
-                (
-                <NumberFormat
-                  value={props.packets_lost.percentage}
-                  displayType={"text"}
-                  suffix={"%"}
-                  decimalScale="1"
-                />
-                )
-              </strong>
-            </div>)}
-          </div>
-          <div className="eight wide column">
-            <div className="empty-container">
-              <h1 className="empty-message">
-                <i className="fas fa-info-circle" />
-                <br />
-                pie chart ?
-              </h1>
-            </div>
-          </div>
+          <Table compact="very" celled color="black" collapsing>
+            <Table.Header>
+              <Table.Row textAlign="center">
+                <Table.HeaderCell textAlign="center">Type</Table.HeaderCell>
+                <Table.HeaderCell textAlign="center">#</Table.HeaderCell>
+                <Table.HeaderCell textAlign="center">%</Table.HeaderCell>
+                <Table.HeaderCell textAlign="center"></Table.HeaderCell>
+              </Table.Row>
+            </Table.Header>
+
+            <Table.Body>
+              <Table.Row>
+                <Table.Cell>
+                  <Icon
+                    color="green"
+                    name="arrow alternate circle down"
+                    type="icon"
+                  />
+                  Received
+                </Table.Cell>
+                <Table.Cell textAlign="right">
+                  <strong>
+                    <NumberFormat
+                      value={props.packets_down.total}
+                      displayType={"text"}
+                    />
+                  </strong>
+                </Table.Cell>
+                <Table.Cell textAlign="right">
+                  <strong>
+                    <NumberFormat
+                      value={props.packets_down.percentage}
+                      displayType={"text"}
+                      suffix={"%"}
+                      decimalScale="2"
+                    />
+                  </strong>
+                </Table.Cell>
+                <Table.Cell rowSpan="4">
+                  <Chart
+                    options={data.options}
+                    series={data.series}
+                    type="pie"
+                    width="150"
+                  />
+                </Table.Cell>
+              </Table.Row>
+              <Table.Row>
+                <Table.Cell>
+                  <Icon
+                    color="orange"
+                    name="arrow alternate circle up"
+                    type="icon"
+                  />
+                  Sent
+                </Table.Cell>
+                <Table.Cell textAlign="right">
+                  <strong>
+                    <NumberFormat
+                      value={props.packets_up.total}
+                      displayType={"text"}
+                    />
+                  </strong>
+                </Table.Cell>
+                <Table.Cell textAlign="right">
+                  <strong>
+                    <NumberFormat
+                      value={props.packets_up.percentage}
+                      displayType={"text"}
+                      suffix={"%"}
+                      decimalScale="2"
+                    />
+                  </strong>
+                </Table.Cell>
+              </Table.Row>
+              {props.type == "device" && (
+                <Table.Row>
+                  <Table.Cell>
+                    <Icon
+                      color="grey"
+                      name="exclamation triangle"
+                      type="icon"
+                    />
+                    Lost
+                  </Table.Cell>
+                  <Table.Cell textAlign="right">
+                    <strong>
+                      <NumberFormat
+                        value={props.packets_lost.total}
+                        displayType={"text"}
+                      />
+                    </strong>
+                  </Table.Cell>
+                  <Table.Cell textAlign="right">
+                    <strong>
+                      <NumberFormat
+                        value={props.packets_lost.percentage}
+                        displayType={"text"}
+                        suffix={"%"}
+                        decimalScale="2"
+                      />
+                    </strong>
+                  </Table.Cell>
+                </Table.Row>
+              )}
+            </Table.Body>
+          </Table>
         </Popup.Content>
       </Popup>
     </span>
