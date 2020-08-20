58ad9d562ae09a43eb48c54b845eb08699607c73
Christian <c.pfarher@gmail.com>
2020-08-20 15:02:47 -0300

    add dummy data for graphs

--- src/stores/resource_ussage.store.js
+++ src/stores/resource_ussage.store.js
@@ -66,6 +66,51 @@ class ResourceUssageStore {
     return { data: data };
   }
 
+  @action getDummyDataForGraphs() {
+    return {
+      byType: {
+        types: [
+          { type: "gateway", qty: 5, filter_key: 'id', filter_value: '1' }, 
+          { type: "device", qty: 95, filter_key: 'id', filter_value: '2' }
+        ],
+        total: 100,
+      },
+      byStauts: {
+        status: [
+          { type: "connected", qty: 5, filter_key: 'id', filter_value: '1'  },
+          { type: "disconnected", qty: 95, filter_key: 'id', filter_value: '1'  },
+        ],
+        total: 100,
+      },
+      bySignalStrength: {
+        signalStrengths: [
+          { qty: 17, text: "0 to -50", unit: "dBm", text: "EXCELLENT", filter_key: 'id', filter_value: '1' },
+          { qty: 17, text: "-50 to -75", unit: "dBm", text: "GREAT", filter_key: 'id', filter_value: '2' },
+          { qty: 17, text: "-75 to -100", unit: "dBm", text: "OKAY", filter_key: 'id', filter_value: '3' },
+          { qty: 17, text: "-100 to -110", unit: "dBm", text: "WEAK", filter_key: 'id', filter_value: '4' },
+          { qty: 17, text: "-110 to -120", unit: "dBm", text: "UNUSABLE", filter_key: 'id', filter_value: '5' },
+          { qty: 17, text: "-120 to -130", unit: "dBm", text: "DISCONNECTED", filter_key: 'id', filter_value: '6' },
+        ],
+        total: 100,
+      },
+      byPacketLost: {
+        packet_losts: [
+          { qty: 10, text: "between 0 and 10%", filter_key: 'id', filter_value: '1' },
+          { qty: 60, text: "between 10 and 20%" , filter_key: 'id', filter_value: '2'}, 
+          { qty: 110, text: "between 20 and 30%", filter_key: 'id', filter_value: '3' },
+          { qty: 160, text: "between 30 and 40%", filter_key: 'id', filter_value: '4' },
+          { qty: 210, text: "between 40 and 50%", filter_key: 'id', filter_value: '5' },
+          { qty: 260, text: "between 50 and 60%", filter_key: 'id', filter_value: '6' },
+          { qty: 310, text: "between 60 and 70%", filter_key: 'id', filter_value: '7' },
+          { qty: 360, text: "between 70 and 80%", filter_key: 'id', filter_value: '8' },
+          { qty: 410, text: "between 80 and 90%", filter_key: 'id', filter_value: '9' },
+          { qty: 460, text: "between 90 and 100%", filter_key: 'id', filter_value: '10' }
+        ],
+        total: 100
+      }
+    };
+  }
+
   @action getDummyData(){
     let data = [
       {
