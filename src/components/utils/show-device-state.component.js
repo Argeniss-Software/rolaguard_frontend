b31ab1a7dcd73bc7745487d7fc5ac26e9a763008
Christian <c.pfarher@gmail.com>
2020-08-21 17:31:02 -0300

    add show popup prop to device state

--- src/components/utils/show-device-state.component.js
+++ src/components/utils/show-device-state.component.js
@@ -9,14 +9,28 @@ const ShowDeviceState = (props) => {
   const statusText = props.state && props.state === connectedState ? connectedText : disconnectedText;
   const colorStatus = props.state && props.state === connectedState ? "green" : "red";
 
-  return (
-    <Popup trigger={<Icon color={colorStatus} name="circle" />}>
-      <Popup.Header>Asset Status</Popup.Header>
-      <Popup.Content>
-        <Icon color={colorStatus} name="circle" /> {statusText}
-      </Popup.Content>
-    </Popup>
-  );
+  if (props.showPopup) {
+    return (
+      <Popup trigger={<Icon color={colorStatus} name="circle" />}>
+        <Popup.Header>Asset Status</Popup.Header>
+        <Popup.Content>
+          <Icon color={colorStatus} name="circle" /> {statusText}
+        </Popup.Content>
+      </Popup>
+    );
+  } else {
+    if (props.showLabel) {
+      return (
+        <React.Fragment>
+          <Icon color={colorStatus} name="circle" />
+          <strong>{statusText.toUpperCase()}</strong>
+        </React.Fragment>
+      );
+    } else {
+      return <Icon color={colorStatus} name="circle" />
+    }
+    
+  }
 };
 
 export default ShowDeviceState;
