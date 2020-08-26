203dc6392d237d4b17724bcfad2e7b623c161984
Christian <c.pfarher@gmail.com>
2020-08-26 08:06:16 -0300

    extract gateway graph to component

--- src/components/resource-usage/graphs/resource-usage.graph.component.js
+++ src/components/resource-usage/graphs/resource-usage.graph.component.js
@@ -1,24 +1,9 @@
 import React, { useState } from "react";
 import { Segment, Grid, Loader } from "semantic-ui-react";
-import Pie from "../../visualizations/Pie";
 import ResourceUsageGraphStatusComponent from "./resource-usage.graph.status.component";
+import ResourceUsageGraphGatewaysComponent from "./resource-usage.graph.gateways.component";
 
 const ResourceUsageGraphComponent = (props) => {
-  const types = [
-    {
-      label: "GATEWAY",
-      percentage: 0.1,
-      value: 50,
-      color: "#103350",
-    },
-    {
-      label: "DEVICES",
-      selected: true,
-      percentage: 0.8,
-      value: 8,
-      color: "#1F77B4",
-    },
-  ];
   //const storeDummyData=getDummyDataForGraphs();
 
   return (
@@ -31,9 +16,11 @@ const ResourceUsageGraphComponent = (props) => {
             tablet={8}
             computer={4}
           >
-            <ResourceUsageGraphStatusComponent
-              props={props}
-            ></ResourceUsageGraphStatusComponent>
+            {
+              <ResourceUsageGraphStatusComponent
+                props={props}
+              ></ResourceUsageGraphStatusComponent>
+            }
           </Grid.Column>
           <Grid.Column
             className="data-container-box pl0 pr0"
@@ -41,18 +28,9 @@ const ResourceUsageGraphComponent = (props) => {
             tablet={8}
             computer={4}
           >
-            <div className="box-data">
-              <h5 className="visualization-title">BY TYPE</h5>
-              <Loader active={props.isGraphsLoading || props.isStatusLoading} />
-              {
-                <Pie
-                  isLoading={props.isGraphsLoading}
-                  data={types}
-                  type={"types"}
-                  handler={props.handleItemSelected}
-                />
-              }
-            </div>
+            <ResourceUsageGraphGatewaysComponent
+              props={props}
+            ></ResourceUsageGraphGatewaysComponent>
           </Grid.Column>
           <Grid.Column
             className="data-container-box pl0 pr0"
