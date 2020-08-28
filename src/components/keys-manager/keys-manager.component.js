5a4a9dbb8cb97c3f454e31dd37cb0aab351b8d2e
Miguel Storani <miguelignaciostorani@gmail.com>
2020-08-28 18:15:42 -0300

    apply format to files

--- src/components/keys-manager/keys-manager.component.js
+++ src/components/keys-manager/keys-manager.component.js
@@ -1,11 +1,18 @@
 import * as React from "react";
 import { MobXProviderContext, observer } from "mobx-react";
-import { Table, Icon, Input, Button, Message, Form, Divider } from "semantic-ui-react";
+import {
+  Table,
+  Icon,
+  Button,
+  Message,
+  Form,
+  Divider,
+} from "semantic-ui-react";
 import Validation from "../../util/validation";
 import EmptyComponent from "../utils/empty.component";
-import LoaderComponent from "../utils/loader.component"
+import LoaderComponent from "../utils/loader.component";
 
-import "./keys-manager.component.css"
+import "./keys-manager.component.css";
 
 const KeysManager = (props) => {
   const { keysStore } = React.useContext(MobXProviderContext);
@@ -18,7 +25,7 @@ const KeysManager = (props) => {
 
   const handleInput = (input) => {
     const key = input.target.value;
-    if(Validation.isValidKeyIncomplete(key)){
+    if (Validation.isValidKeyIncomplete(key)) {
       setNewKey(key);
       setDisabled(!Validation.isValidKey(key));
     }
@@ -36,102 +43,134 @@ const KeysManager = (props) => {
 
   return (
     <React.Fragment>
-      { keysStore.hasError &&
-        <div style={{textAlign:"center"}}>
-          <Message visible error header="Oops!" style={{maxWidth: '100%'}}>
-            Something went wrong! Try again later or <a href="#" onClick={() => keysStore.getKeysFromAPI()}>reload this component</a>.
+      {keysStore.hasError && (
+        <div style={{ textAlign: "center" }}>
+          <Message visible error header="Oops!" style={{ maxWidth: "100%" }}>
+            Something went wrong! Try again later or{" "}
+            <a href="#" onClick={() => keysStore.getKeysFromAPI()}>
+              reload this component
+            </a>
+            .
           </Message>
         </div>
-      }
+      )}
 
-      { !keysStore.hasError &&
-      <div className="keys-manager-top">
-        <div className="keys-manager-wrapper">
-          <Form onSumbit={saveKey}>
-          <Table width="570px" className="animated fadeIn keys-table"  compact="very">
-            <Table.Header>
-              <Table.HeaderCell></Table.HeaderCell>
-              <Table.HeaderCell><p style={{width:"430px", textAlign:"center"}}>KEYS</p></Table.HeaderCell>
-              <Table.HeaderCell></Table.HeaderCell>
-            </Table.Header>
-            <Table.Body>
+      {!keysStore.hasError && (
+        <div className="keys-manager-top">
+          <div className="keys-manager-wrapper">
+            <Form onSumbit={saveKey}>
+              <Table
+                width="570px"
+                className="animated fadeIn keys-table"
+                compact="very"
+              >
+                <Table.Header>
+                  <Table.HeaderCell></Table.HeaderCell>
+                  <Table.HeaderCell>
+                    <p style={{ width: "430px", textAlign: "center" }}>KEYS</p>
+                  </Table.HeaderCell>
+                  <Table.HeaderCell></Table.HeaderCell>
+                </Table.Header>
+                <Table.Body>
+                  {/** Keys list */}
+                  {!!keysStore.count &&
+                    keysStore.keys.map((item, index) => (
+                      <Table.Row key={index}>
+                        <Table.Cell className="icon-cell">
+                          <i className="fas fa-key" />
+                        </Table.Cell>
+                        <Table.Cell className="key-cell">
+                          {item.key && item.key.toUpperCase()}
+                        </Table.Cell>
+                        <Table.Cell className="action-cell">
+                          <i
+                            className="fas fa-trash"
+                            onClick={() => deleteKey(item.key)}
+                          />
+                        </Table.Cell>
+                      </Table.Row>
+                    ))}
 
-              {/** Keys list */}
-              {!!keysStore.count &&
-                keysStore.keys.map((item, index) => (
-                  <Table.Row key={index}>
-                    <Table.Cell className="icon-cell">
-                      <i className="fas fa-key" />
+                  {/** Empty component */}
+                  {!keysStore.count && !keysStore.isLoading && (
+                    <React.Fragment>
+                      <Table.Row>
+                        <Table.Cell />
+                        <Table.Cell>
+                          <EmptyComponent
+                            style={{ width: "570px" }}
+                            className="animated fadeIn"
+                            emptyMessage="No keys found"
+                          />
+                        </Table.Cell>
+                        <Table.Cell />
+                      </Table.Row>
+
+                      <Table.Row>
+                        <Table.Cell colSpan="3">
+                          <Divider />
+                        </Table.Cell>
+                      </Table.Row>
+                    </React.Fragment>
+                  )}
+
+                  {/** Loader component */}
+                  {keysStore.isLoading && (
+                    <React.Fragment>
+                      <Table.Row>
+                        <Table.Cell />
+                        <Table.Cell>
+                          <LoaderComponent
+                            className="animated fadeIn"
+                            loadingMessage="loading..."
+                          />
+                        </Table.Cell>
+                        <Table.Cell />
+                      </Table.Row>
+
+                      <Table.Row>
+                        <Table.Cell colSpan="3">
+                          <Divider />
+                        </Table.Cell>
+                      </Table.Row>
+                    </React.Fragment>
+                  )}
+
+                  {/** Create new key input */}
+                  <Table.Row>
+                    <Table.Cell className="icon-cell imput-row">
+                      <Icon name="plus" />
                     </Table.Cell>
-                    <Table.Cell className="key-cell">{item.key && item.key.toUpperCase()}</Table.Cell>
+                    <Table.Cell className="key-cell">
+                      <Form.Input
+                        autocomplete="off"
+                        required
+                        fluid
+                        transparent
+                        placeholder="Add new key"
+                        onChange={handleInput}
+                        value={newKey}
+                      />
+                    </Table.Cell>
                     <Table.Cell className="action-cell">
-                      <i className="fas fa-trash" onClick={() => deleteKey(item.key)} />
+                      <Button
+                        disabled={disabled}
+                        onClick={(e) => {
+                          e.preventDefault();
+                          saveKey();
+                        }}
+                      >
+                        save
+                      </Button>
                     </Table.Cell>
                   </Table.Row>
-                ))
-              }
-
-              {/** Empty component */}
-              {!keysStore.count && !keysStore.isLoading &&
-              <React.Fragment>
-                <Table.Row>
-                  <Table.Cell/>
-                  <Table.Cell>
-                    <EmptyComponent style={{width:"570px"}} className="animated fadeIn" emptyMessage="No keys found" />
-                  </Table.Cell>
-                  <Table.Cell/>
-                </Table.Row>
-                
-                <Table.Row>
-                  <Table.Cell colSpan="3">
-                    <Divider/>
-                  </Table.Cell>
-                </Table.Row>
-              </React.Fragment>
-              }
-
-              {/** Loader component */}
-              {keysStore.isLoading &&
-              <React.Fragment>
-                <Table.Row>
-                  <Table.Cell/>
-                  <Table.Cell>
-                    <LoaderComponent className="animated fadeIn" loadingMessage="loading..." />
-                  </Table.Cell>
-                  <Table.Cell/>
-                </Table.Row>
-
-                <Table.Row>
-                  <Table.Cell colSpan="3">
-                    <Divider/>
-                  </Table.Cell>
-                </Table.Row>
-              </React.Fragment>
-              }
-
-              {/** Create new key input */}
-              <Table.Row>
-                <Table.Cell className="icon-cell imput-row">
-                  <Icon name="plus" />
-                </Table.Cell>
-                <Table.Cell className="key-cell">
-                  <Form.Input autocomplete="off" required fluid transparent placeholder="Add new key" onChange={handleInput} value={newKey}/>
-                </Table.Cell>
-                <Table.Cell className="action-cell">
-                  <Button disabled={disabled} onClick={(e) => {e.preventDefault(); saveKey();}}>
-                    save
-                  </Button>
-                </Table.Cell>
-              </Table.Row>
-
-            </Table.Body>
-          </Table>
-          </Form>
+                </Table.Body>
+              </Table>
+            </Form>
+          </div>
         </div>
-      </div>
-      }
+      )}
     </React.Fragment>
-
   );
 };
 
--- src/stores/keys.store.js
+++ src/stores/keys.store.js
@@ -5,37 +5,37 @@ import * as HttpStatus from "http-status-codes";
 
 class KeysStore {
   @observable keys = [];
-  @observable count = 0
+  @observable count = 0;
   @observable keysLoading = false;
   @observable hasError = false;
   @observable errorMessage = "";
   @observable isLoading = true;
 
   getHeaders() {
-    return {headers: { Authorization: "Bearer " + AuthStore.access_token }};
+    return { headers: { Authorization: "Bearer " + AuthStore.access_token } };
   }
 
   @action getKeysFromAPI() {
     this.isLoading = true;
     const headers = this.getHeaders();
 
-    API.get('app_keys', headers)
-    .then((response) => {
-      if (response.status === HttpStatus.OK) {
-        this.keys = response.data.keys;
-        this.count = response.data.count;
-        this.hasError = false;
-      } else {
+    API.get("app_keys", headers)
+      .then((response) => {
+        if (response.status === HttpStatus.OK) {
+          this.keys = response.data.keys;
+          this.count = response.data.count;
+          this.hasError = false;
+        } else {
+          this.hasError = true;
+          this.errorMessage = response.data;
+        }
+        this.isLoading = false;
+      })
+      .catch(() => {
         this.hasError = true;
-        this.errorMessage = response.data;
-      }
-      this.isLoading = false;
-    })
-    .catch(() => {
-      this.hasError = true;
-      this.errorMessage = "Network error"
-      this.isLoading = false;
-    });
+        this.errorMessage = "Network error";
+        this.isLoading = false;
+      });
   }
 
   @action addKey(key) {
@@ -44,10 +44,11 @@ class KeysStore {
     */
 
     const headers = this.getHeaders();
-    const param = { keys: [ key ] };
+    const param = { keys: [key] };
 
-    return API.post('app_keys', param, headers)
-    .then(() => this.getKeysFromAPI());
+    return API.post("app_keys", param, headers).then(() =>
+      this.getKeysFromAPI()
+    );
   }
 
   @action deleteKey(key) {
@@ -55,11 +56,12 @@ class KeysStore {
       @param key: string with 16 bytes hexadecimal value (32 characters)
     */
 
-   const headers = this.getHeaders();
-   const param = { keys: [ key ] };
+    const headers = this.getHeaders();
+    const param = { keys: [key] };
 
-   return API.deleteWithBody('app_keys', param, headers)
-   .then(() => this.getKeysFromAPI())
+    return API.deleteWithBody("app_keys", param, headers).then(() =>
+      this.getKeysFromAPI()
+    );
   }
 }
 
