19db6f0ae4c9a8d8f7462c895e6a4c9c1a38428a
Christian <c.pfarher@gmail.com>
2020-08-28 09:37:51 -0300

    integrate store on resource usage graph

--- src/stores/resource_usage.store.js
+++ src/stores/resource_usage.store.js
@@ -1,7 +1,8 @@
-import { observable, action } from "mobx";
+import { observable, action, runInAction, computed } from "mobx";
 import AuthStore from "./auth.store";
 import API from "../util/api";
 import _ from "lodash";
+import ColorUtil from "../util/colors";
 
 class ResourceUsageStore {
   @observable criteria = {
@@ -9,13 +10,53 @@ class ResourceUsageStore {
     type: null, // device or gateway
     status: null, // connected or disconnected
     gateways: [],
+    signal_strength: [],
+    packet_lost_range: { from: 0, to: 100 },
   };
+
+  @observable model = {
+    list: [],
+    isLoading: false,
+    totalList: 0,
+    totalPages: 0,
+    activePage: 1,
+    pageSize: 20,
+  };
+
+  @action setModelLoading(val) {
+    this.model.isLoading = val;
+  }
+
+  @action setTotalList(val) {
+    this.model.totalList = val;
+  }
+
+  @action setTotalPages(val) {
+    this.model.totalPages = val;
+  }
+
+  @action setActivePage(val) {
+    this.model.activePage = val;
+    this.getDataListFromApi();
+  }
+  @action setPageSize(val) {
+    this.model.pageSize = val;
+  }
+
+  @action setList(data) {
+    this.model.list = data;
+  }
+
+  @computed get getTotalList() {
+    return this.model.list.length;
+  }
   /**********************************************************/
   /**********************************************************/
   @observable statusGraph = {
     // keep data related of status graph on resoruce usage dashbaord
     seriesSelected: null,
     isLoading: false,
+    series: [],
   };
   @action getStatusLoading() {
     return this.statusGraph.isLoading;
@@ -29,12 +70,155 @@ class ResourceUsageStore {
   @action setStatusGraphSeriesSelected(data) {
     this.statusGraph.seriesSelected = data;
   }
+  @action setStatusGraphSeries(series) {
+    this.statusGraph.series = series;
+  }
+
+  @action getDataListFromApi = () => {
+    // get list of resource usage by criteria
+    this.setModelLoading(true);
+    const self = this;
+    const assetsPromise = this.getAssets(
+      { page: this.model.activePage, size: this.model.pageSize },
+      this.getCriteria()
+    );
+    Promise.all([assetsPromise]).then((response) => {
+      self.setTotalList(response[0].data.total_items);
+      self.setTotalPages(response[0].data.total_pages);
+      self.setList(this.formatApiData(response[0].data.assets));
+    });
+    this.setModelLoading(false);
+  };
+
+  @action getDataStatusFromApi = () => {
+    this.setStatusLoading(true);
+    const statusPromise = this.getAssetsCountStatus();
+
+    Promise.all([statusPromise]).then((response) => {
+      let total = response[0].data.total_count;
+      let apiSeries = response[0].data.groups.map((e, index) => {
+        return {
+          label: e.name.toUpperCase(),
+          id: e.id,
+          selected: !_.isEmpty(this.getStatusGraphSeriesSelected())
+            ? e.id === this.getStatusGraphSeriesSelected().id
+            : false,
+          percentage: !_.isEmpty(this.getStatusGraphSeriesSelected())
+            ? 1
+            : total > 0
+            ? e.count / total
+            : e.count,
+          value: e.count,
+          color: index === 0 ? "#21ba45" : "#F05050",
+        };
+      });
+      if (!_.isEmpty(this.getStatusGraphSeriesSelected())) {
+        apiSeries = apiSeries.filter((item) => item.selected);
+      }
+      runInAction(() => {
+        this.statusGraph.series = apiSeries;
+      });
+      //this.setStatusGraphSeries(apiSeries);
+    });
+    this.setStatusLoading(false);
+
+    if (!_.isEmpty(this.getStatusGraphSeriesSelected())) {
+      this.statusGraph.series.forEach((e) => {
+        return (e.selected = e.id === this.getStatusGraphSeriesSelected().id);
+      });
+    }
+  };
+
+  @action getDataGatewaysFromApi = () => {
+    debugger;
+    this.setGatewaysLoading(true);
+    const statusPromise = this.getAssetsCountGateways();
+
+    Promise.all([statusPromise]).then((response) => {
+      let total = response[0].data.total_count;
+      let apiSeries = response[0].data.groups.map((e, index) => {
+        return {
+          label: e.name.toUpperCase(),
+          id: e.id,
+          selected: !_.isEmpty(this.getGatewayGraphSeriesSelected())
+            ? e.id === this.getGatewayGraphSeriesSelected().id
+            : false,
+          percentage: !_.isEmpty(this.getGatewayGraphSeriesSelected())
+            ? 1
+            : total > 0
+            ? e.count / total
+            : e.count,
+          value: e.count,
+          color: ColorUtil.getByIndex(index),
+        };
+      });
+      if (!_.isEmpty(this.getGatewayGraphSeriesSelected())) {
+        apiSeries = apiSeries.filter((item) => item.selected);
+      }
+      //this.setGatewayGraphSeries(apiSeries);
+      runInAction(() => {
+        this.gatewaysGraph.series = apiSeries;
+      });
+    });
+
+    if (!_.isEmpty(this.getGatewayGraphSeriesSelected())) {
+      //_.pluck(this.getGatewayGraphSeriesSelected(), "id")
+      if (!_.isEmpty(this.criteria.gateways)) {
+        let gatewaysIdSelected = this.criteria.gateways.map((e) => e.id);
+        this.gatewaysGraph.series.forEach((e) => {
+          return (e.selected = gatewaysIdSelected.includes(e.id));
+        });
+      }
+    }
+    this.setGatewaysLoading(false);
+  };
+
+  @action getDataPacketsLostFromApi = () => {
+    // @ todo: to implement!
+    this.setStatusLoading(true);
+    const statusPromise = this.getAssetsCountStatus();
+
+    Promise.all([statusPromise]).then((response) => {
+      let total = response[0].data.total_count;
+      let apiSeries = response[0].data.groups.map((e, index) => {
+        return {
+          label: e.name.toUpperCase(),
+          id: e.id,
+          selected: !_.isEmpty(this.getStatusGraphSeriesSelected())
+            ? e.id === this.getStatusGraphSeriesSelected().id
+            : false,
+          percentage: !_.isEmpty(this.getStatusGraphSeriesSelected())
+            ? 1
+            : total > 0
+            ? e.count / total
+            : e.count,
+          value: e.count,
+          color: index === 0 ? "#21ba45" : "#F05050",
+        };
+      });
+      if (!_.isEmpty(this.getStatusGraphSeriesSelected())) {
+        apiSeries = apiSeries.filter((item) => item.selected);
+      }
+      runInAction(() => {
+        this.statusGraph.series = apiSeries;
+      });
+      //this.setStatusGraphSeries(apiSeries);
+    });
+    this.setStatusLoading(false);
+
+    if (!_.isEmpty(this.getStatusGraphSeriesSelected())) {
+      this.statusGraph.series.forEach((e) => {
+        return (e.selected = e.id === this.getStatusGraphSeriesSelected().id);
+      });
+    }
+  };
   /**********************************************************/
   /**********************************************************/
   @observable gatewaysGraph = {
     // keep data related of gateway graph on resoruce usage dashbaord
     seriesSelected: [],
     isLoading: false,
+    series: [],
   };
   @action getGatewaysLoading() {
     return this.gatewaysGraph.isLoading;
@@ -45,10 +229,17 @@ class ResourceUsageStore {
   @action getGatewayGraphSeriesSelected() {
     return this.gatewaysGraph.seriesSelected;
   }
+  @action setGatewayGraphSeries(series) {
+    this.gatewaysGraph.series = series;
+  }
+
   @action setGatewayGraphSeriesSelected(data) {
-    let elementFoundIndex = this.gatewaysGraph.seriesSelected.findIndex((e) => {
-      return e.id === data.id;
-    });
+    let elementFoundIndex = -1;
+    if (!_.isEmpty(this.gatewaysGraph.seriesSelected) && !_.isEmpty(data)) {
+      elementFoundIndex = this.gatewaysGraph.seriesSelected.findIndex((e) => {
+        return e.id === data.id;
+      });
+    }
     if (elementFoundIndex != -1) {
       //remove filter
       this.gatewaysGraph.seriesSelected.splice(elementFoundIndex, 1);
@@ -61,7 +252,7 @@ class ResourceUsageStore {
     } else {
       // add filter
       this.gatewaysGraph.seriesSelected.push(data);
-      this.criteria.gateways.push(data);
+      this.setCriteria({ gateways: data });
     }
     //    this.gatewaysGraph.seriesSelected = {...this.gatewaysGraph.seriesSelected, ...data}
   }
@@ -72,46 +263,73 @@ class ResourceUsageStore {
   }
 
   @action deleteCriteria(data) {
-    let deleteCriteria = {};
-
     if (_.isEmpty(data)) {
       // clear all
-      this.setStatusGraphSeriesSelected(null); // clean selected status element on status graph!
-      this.setGatewayGraphSeriesSelected(null); // clean selected status element on status graph!
-
-      this.criteria = {
+      this.setStatusGraphSeriesSelected({}); // clean selected status element on status graph!
+      this.setGatewayGraphSeriesSelected({}); // clean selected status element on status graph!
+      this.setCriteria({
         type: null, // device or gateway
         status: null, // connected or disconnected
-        gateway: [],
-      };
+        gateways: [],
+        signal_strength: [],
+        packet_lost_range: { from: 0, to: 100 },
+      });
     } else {
       let keyCriteriaToDelete = _.keys(data)[0];
+      debugger
       switch (keyCriteriaToDelete) {
         case "status":
-          this.setStatusGraphSeriesSelected(null); // clean selected status element on status graph!
-          this.criteria.status = null;
+          this.setStatusGraphSeriesSelected({}); // clean selected status element on status graph!
+          this.setStatusGraphSeriesSelected({});
+          this.setCriteria({ status: null });
           break;
-        case "types":
-          this.criteria.type = null;
+        case "type":
+          this.setCriteria({ type: null });
           break;
         case "gateways":
           this.setGatewayGraphSeriesSelected(data[keyCriteriaToDelete]);
-          let foundItemToDelete = this.criteria.gateways.findIndex(
-            (e) => e.id === data[keyCriteriaToDelete].id
-          );
-          if (foundItemToDelete != -1) {
-            this.criteria.gateway.slice(foundItemToDelete, 1);
-          }
+          this.setCriteria({ gateways: data.gateways });
+          break;
+        case "packet_lost_range":
+          this.setCriteria({ packet_lost_range: { from: 0, to: 100 } });
           break;
       }
     }
   }
 
+  @action loadDataFromApis() {
+    this.getDataListFromApi();
+    this.getDataStatusFromApi();
+    this.getDataGatewaysFromApi();
+  }
   @action setCriteria(data) {
-    this.criteria = {
-      ...this.criteria,
-      ...(_.isFunction(data) ? data.call() : data),
-    };
+    let keyCriteriaToDelete = _.keys(data)[0];
+    debugger;
+    switch (keyCriteriaToDelete) {
+      case "status":
+      case "type":
+        this.criteria = {
+          ...this.criteria,
+          ...(_.isFunction(data) ? data.call() : data),
+        };
+        break;
+      case "gateways":
+        let foundItemToDelete = this.criteria.gateways.findIndex(
+          (e) => e.id === data[_.keys(data)[0]].id
+        );
+        if (foundItemToDelete != -1 && this.criteria.gateways.length > 0) {
+          // delete gateway to filter
+          this.criteria.gateways.remove(_.keys(data)[0]);
+        } else {
+          // add gateway to filter
+          this.criteria.gateways.push(data.gateways);
+        }
+        break;
+      case "packet_lost_range":
+        this.criteria.packet_lost_range.from = data.packet_lost_range.from;
+        this.criteria.packet_lost_range.to = data.packet_lost_range.to;
+    }
+    this.loadDataFromApis();
   }
   /**********************************************************/
   /**********************************************************/
@@ -121,13 +339,18 @@ class ResourceUsageStore {
 
   @action getAssets(pagination, criteria) {
     const { page, size } = pagination || {};
-    const { status, type, gateways } = this.criteria || {};
+    const { status, type, gateways, packet_lost_range } = this.criteria || {};
 
     const headers = this.getHeaders();
     const params = {
       ...(status && { asset_status: this.criteria.status }),
       ...(type && { asset_type: this.criteria.type }),
       ...(gateways && { gateway_ids: gateways.map((e) => e.id) }),
+      ...(packet_lost_range && {
+        min_packet_loss: packet_lost_range.from,
+        max_packet_loss: packet_lost_range.to,
+      }),
+
       page,
       size,
     };
@@ -136,24 +359,32 @@ class ResourceUsageStore {
 
   // return resource usage global status (connected/disconnected)
   @action getAssetsCountStatus(criteria) {
-    const { status, type, gateways } = this.criteria || {};
+    const { status, type, gateways, packet_lost_range } = this.criteria || {};
     const headers = this.getHeaders();
     const params = {
       ...(status && { asset_status: this.criteria.status }),
       ...(type && { asset_type: this.criteria.type }),
       ...(gateways && { gateway_ids: this.criteria.gateways.map((e) => e.id) }),
+      ...(packet_lost_range && {
+        min_packet_loss: packet_lost_range.from,
+        max_packet_loss: packet_lost_range.to,
+      }),
     };
     return API.get(`resource_usage/count/status`, { headers, params });
   }
 
   // return different gateways associated to resource usage
   @action getAssetsCountGateways(criteria) {
-    const { status, type, gateways } = this.criteria || {};
+    const { status, type, gateways, packet_lost_range } = this.criteria || {};
     const headers = this.getHeaders();
     const params = {
       ...(status && { asset_status: this.criteria.status }),
       ...(type && { asset_type: this.criteria.type }),
       ...(gateways && { gateway_ids: this.criteria.gateways.map((e) => e.id) }),
+      ...(packet_lost_range && {
+        min_packet_loss: packet_lost_range.from,
+        max_packet_loss: packet_lost_range.to,
+      }),
     };
 
     return API.get(`resource_usage/count/gateway`, { headers, params });
@@ -310,6 +541,91 @@ class ResourceUsageStore {
     ];
     return data;
   }
+  /*@action
+  getAssets(pagination, criteria) {
+    const { page, size } = pagination || {};
+    const { vendors, gateways, dataCollectors, tags, type} = criteria || {};
+
+    const headers = this.getHeaders();
+    const params = {
+      ...vendors && {'vendors': vendors},
+      ...gateways && {'gateway_ids': gateways},
+      ...dataCollectors && {'data_collector_ids': dataCollectors},
+      ...tags && {'tag_ids': tags},
+      ...type && {'asset_type': type},
+      page,
+      size
+    };
+    return API.get(`resource_usage/list`, { headers, params} );
+  }*/
+
+  /*getAsstesCount(){
+    return this.assetsCount;
+  }
+
+  getPagesCount(){
+    return this.pagesCount;
+  }*/
+
+  /*getDataCollectorsCount(criteria){
+    const { vendors, gateways, dataCollectors, tags, type} = criteria || {};
+
+    const headers = this.getHeaders();
+    const params = {
+      ...vendors && {'vendors': vendors},
+      ...gateways && {'gateway_ids': gateways},
+      ...dataCollectors && {'data_collector_ids': dataCollectors},
+      ...tags && {'tag_ids': tags},
+      ...type && {'asset_type': type},
+    };
+
+    return API.get(`inventory/count/data_collector`, { headers, params} );
+  }
+
+  getGatewaysCount(criteria){
+    const { vendors, gateways, dataCollectors, tags, type} = criteria || {};
+
+    const headers = this.getHeaders();
+    const params = {
+      ...vendors && {'vendors': vendors},
+      ...gateways && {'gateway_ids': gateways},
+      ...dataCollectors && {'data_collector_ids': dataCollectors},
+      ...tags && {'tag_ids': tags},
+      ...type && {'asset_type': type},
+    };
+
+    return API.get(`inventory/count/gateway`, { headers, params} );
+  }
+
+  getVendorsCount(criteria){
+    const { vendors, gateways, dataCollectors, tags, type} = criteria || {};
+
+    const headers = this.getHeaders();
+    const params = {
+      ...vendors && {'vendors': vendors},
+      ...gateways && {'gateway_ids': gateways},
+      ...dataCollectors && {'data_collector_ids': dataCollectors},
+      ...tags && {'tag_ids': tags},
+      ...type && {'asset_type': type},
+    };
+
+    return API.get(`inventory/count/vendor`, { headers, params} );
+  }
+
+  getTagsCount(criteria){
+    const { vendors, gateways, dataCollectors, tags, type} = criteria || {};
+
+    const headers = this.getHeaders();
+    const params = {
+      ...vendors && {'vendors': vendors},
+      ...gateways && {'gateway_ids': gateways},
+      ...dataCollectors && {'data_collector_ids': dataCollectors},
+      ...tags && {'tag_ids': tags},
+      ...type && {'asset_type': type},
+    };
+
+    return API.get(`inventory/count/tag`, { headers, params} );
+  }*/
 }
 const store = new ResourceUsageStore();
 export default store;
