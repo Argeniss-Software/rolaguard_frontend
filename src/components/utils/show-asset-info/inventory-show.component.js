c937d4df513c1ffcf79fd8ba99971d61d7e8e80e
Christian <c.pfarher@gmail.com>
2020-09-07 18:17:04 -0300

    add link to copy to clipboard

--- src/components/utils/show-asset-info/inventory-show.component.js
+++ src/components/utils/show-asset-info/inventory-show.component.js
@@ -24,44 +24,49 @@ const ShowInventory = (props) => {
     <div class="column">
       <h5
         class="ui black inverted top attached header"
-        style={{ backgroundColor: "black", fontSize: "16px" }}
+        style={{ backgroundColor: "black", fontSize: "18px", padding: "15px" }}
       >
-        <div>
-          <ShowDeviceState state={props.inventory.connected} />
-          &nbsp;&nbsp;&nbsp;
-          <ShowDeviceIcon
-            type={
-              props.inventory.type &&
-              !["gateway", "device"].includes(
-                props.inventory.type.toLowerCase().trim()
-              )
-                ? "unknown"
-                : props.inventory.type
-            }
-          ></ShowDeviceIcon>
-          &nbsp;
-          {_.get(props, "inventory.type")
-            ? props.inventory.type.toUpperCase() + ": "
-            : ""}
-          <AssetIdComponent
-            type={props.inventory.type}
-            id={props.inventory.id}
-            hexId={props.inventory.hex_id}
-            showAsLink={false}
-          />
-          &nbsp;&nbsp;&nbsp;
-          <Popup
-            trigger={
-              <ImportanceLabel importance={props.inventory.importance} />
-            }
-          >
-            The importance value indicates the user-defined relevance of the
-            device into the organization. Can be set for each asset in the
-            Inventory section.
-          </Popup>
-        </div>
+        <Grid>
+          <Grid.Column floated="left" width={13}>
+            <ShowDeviceState state={props.inventory.connected} />
+            &nbsp;&nbsp;&nbsp;
+            <ShowDeviceIcon
+              type={
+                props.inventory.type &&
+                !["gateway", "device"].includes(
+                  props.inventory.type.toLowerCase().trim()
+                )
+                  ? "unknown"
+                  : props.inventory.type
+              }
+            ></ShowDeviceIcon>
+            &nbsp;
+            {_.get(props, "inventory.type")
+              ? props.inventory.type.toUpperCase() + ": "
+              : ""}
+            <AssetIdComponent
+              type={props.inventory.type}
+              id={props.inventory.id}
+              hexId={props.inventory.hex_id}
+              showAsLink={false}
+            />
+            &nbsp;&nbsp;&nbsp;
+            <Popup
+              trigger={
+                <ImportanceLabel importance={props.inventory.importance} />
+              }
+            >
+              The importance value indicates the user-defined relevance of the
+              device into the organization. Can be set for each asset in the
+              Inventory section.
+            </Popup>
+          </Grid.Column>
+          <Grid.Column floated="right" width={3}>
+            {props.LayoutHeaderRight}
+          </Grid.Column>
+        </Grid>
       </h5>
-      <Segment attached>
+      <Segment attached>        
         <Grid columns={16} columns="equal">
           <Grid.Column flex key={5}>
             <Table compact striped style={{ height: "100%" }}>
