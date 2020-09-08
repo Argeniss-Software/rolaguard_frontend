8ee9751e7299b7e7e527837679990302049fc8f9
Christian <c.pfarher@gmail.com>
2020-09-08 14:51:18 -0300

    open link of 360 in new page

--- src/components/utils/asset-link.component.js
+++ src/components/utils/asset-link.component.js
@@ -6,7 +6,7 @@ const AssetLinkComponent = (props) => {
   const getLinkToDevice = (title, assetId, type = "device") => {
     const normalizedType = type && type.toLowerCase().trim()
     if (!_.isNull(assetId) && !_.isUndefined(assetId) && ['gateway', 'device'].includes(normalizedType)) {
-      return <a href={`/dashboard/assets/${normalizedType}/${assetId}/view`}>{title}</a>
+      return <a target="_blank" href={`/dashboard/assets/${normalizedType}/${assetId}/view`}>{title}</a>
     } else {
       return title
     }
