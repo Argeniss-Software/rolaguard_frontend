3624728a632ecb54cd1e32afb34df50f59caa843
Christian <c.pfarher@gmail.com>
2020-09-08 16:44:00 -0300

    add loading element

--- src/components/utils/show-asset-info/show-asset-info.component.js
+++ src/components/utils/show-asset-info/show-asset-info.component.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useContext } from "react";
 import { MobXProviderContext } from "mobx-react";
 
-import { Label, Icon, Grid, Segment, Popup} from "semantic-ui-react";
+import { Label, Icon, Grid, Segment, Popup, Dimmer, Loader} from "semantic-ui-react";
 import ShowAlerts from "./alerts-show.component";
 import ShowCurrentIssues from "./current-issues-show.component";
 
@@ -20,6 +20,8 @@ const ShowAssetInfo = (props) => {
   const [current_issues, setCurrentIssues] = useState({});
   const [resource_usage, setResourceUsage] = useState({});
   const [copied, setCopied] = useState(false);
+  const [isLoading, setIsLoading] = useState(true)
+
   useEffect(() => {
       let paramsId = {
         type: props.type,
@@ -41,98 +43,106 @@ const ShowAssetInfo = (props) => {
         setAlerts(response[1].data);
         setCurrentIssues(response[2].data);
         setResourceUsage(response[3].data);
+        setIsLoading(false)
       });
     }, [props.id, props.type]);
-
-  return (
-    <React.Fragment>
-      <Grid columns={16} columns="equal" style={{ marginTop: "1em" }}>
-        <ShowInventory
-          inventory={inventory}
-          LayoutHeaderRight={
-            <div class="pull-right">
-              <span
-                style={{
-                  color: "white",
-                  fontStyle: "italic",
-                  fontSize: "12px",
-                }}
-              >
-                {copied ? "copied to clipboard... " : ""}
-              </span>
-              <Popup
-                basic
-                trigger={
-                  <CopyToClipboard
-                    text={window.location.href}
-                    onCopy={() => setCopied(true)}
+    if (isLoading) {
+      return (
+      <Dimmer active inverted>
+        <Loader inverted content="Loading" />
+      </Dimmer>
+      )
+    } else {
+      return (
+        <React.Fragment>
+          <Grid columns={16} columns="equal" style={{ marginTop: "1em" }}>
+            <ShowInventory
+              inventory={inventory}
+              LayoutHeaderRight={
+                <div class="pull-right">
+                  <span
+                    style={{
+                      color: "white",
+                      fontStyle: "italic",
+                      fontSize: "12px",
+                    }}
                   >
-                    <Icon
-                      name="external share"
-                      inverted
-                      style={{ cursor: "pointer" }}
-                    />
-                  </CopyToClipboard>
-                }
-                content="Press here to copy the link to clipboard for sharing"
-              />
-            </div>
-          }
-        />
-      </Grid>
-      <Grid columns={16} columns="equal">
-        <Grid.Row>
-          <Grid.Column flex key={16}>
-            <h5
-              class="ui inverted top attached header blue segment"
-              style={{ height: "44px" }}
-            >
-              NETWORK OVERVIEW
-            </h5>
-            <Segment attached>
-              <ShowResourceUsage asset={resource_usage} />
-            </Segment>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-      <Grid columns={16} columns="equal">
-        <Grid.Row>
-          <Grid.Column flex key={8}>
-            <h5
-              class="ui inverted top attached header"
-              style={{ height: "44px" }}
-            >
-              CURRENT ISSUES{" "}
-              {current_issues && current_issues.total_items > 0 && (
-                <Label color="yellow">{current_issues.total_items}</Label>
-              )}
-            </h5>
-            <Segment attached>
-              <ShowCurrentIssues currentIssues={current_issues} />
-            </Segment>
-          </Grid.Column>
+                    {copied ? "copied to clipboard... " : ""}
+                  </span>
+                  <Popup
+                    basic
+                    trigger={
+                      <CopyToClipboard
+                        text={window.location.href}
+                        onCopy={() => setCopied(true)}
+                      >
+                        <Icon
+                          name="external share"
+                          inverted
+                          style={{ cursor: "pointer" }}
+                        />
+                      </CopyToClipboard>
+                    }
+                    content="Press here to copy the link to clipboard for sharing"
+                  />
+                </div>
+              }
+            />
+          </Grid>
+          <Grid columns={16} columns="equal">
+            <Grid.Row>
+              <Grid.Column flex key={16}>
+                <h5
+                  class="ui inverted top attached header blue segment"
+                  style={{ height: "44px" }}
+                >
+                  NETWORK OVERVIEW
+                </h5>
+                <Segment attached>
+                  <ShowResourceUsage asset={resource_usage} />
+                </Segment>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+          <Grid columns={16} columns="equal">
+            <Grid.Row>
+              <Grid.Column flex key={8}>
+                <h5
+                  class="ui inverted top attached header"
+                  style={{ height: "44px" }}
+                >
+                  CURRENT ISSUES{" "}
+                  {current_issues && current_issues.total_items > 0 && (
+                    <Label color="yellow">{current_issues.total_items}</Label>
+                  )}
+                </h5>
+                <Segment attached>
+                  <ShowCurrentIssues currentIssues={current_issues} />
+                </Segment>
+              </Grid.Column>
 
-          <Grid.Column flex key={8}>
-            <h5
-              class="ui inverted top attached header"
-              style={{ height: "44px" }}
-            >
-              ALERTS{" "}
-              {alerts && alerts.total_items > 0 && (
-                <Label color="red">{alerts.total_items}</Label>
-              )}
-            </h5>
-            <Segment attached>
-              <ShowAlerts
-                alerts={alerts.alerts}
-                totalItems={alerts.total_items}
-              />
-            </Segment>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-    </React.Fragment>
-  );
+              <Grid.Column flex key={8}>
+                <h5
+                  class="ui inverted top attached header"
+                  style={{ height: "44px" }}
+                >
+                  ALERTS{" "}
+                  {alerts && alerts.total_items > 0 && (
+                    <Label color="red">{alerts.total_items}</Label>
+                  )}
+                </h5>
+                <Segment attached>
+                  <ShowAlerts
+                    alerts={alerts.alerts}
+                    totalItems={alerts.total_items}
+                  />
+                </Segment>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </React.Fragment>
+      );
+    }
 };
 
 export default ShowAssetInfo;
