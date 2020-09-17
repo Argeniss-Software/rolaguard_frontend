4d3a996202c8703b1971d435e739f206c322ab3c
Christian <c.pfarher@gmail.com>
2020-09-17 15:03:28 -0300

    delete comments

--- src/components/utils/show-asset-info/alert-timeline-graph.component.js
+++ src/components/utils/show-asset-info/alert-timeline-graph.component.js
@@ -1,13 +1,11 @@
 import React, { useEffect, useState, useContext } from "react";
-import { Button, Grid, Segment, Popup } from "semantic-ui-react";
 import { MobXProviderContext } from "mobx-react";
 import "./alert-timeline-graph.component.css";
-//import ReactDOM from "react-dom";
 import TimeLineGraph from "../../visualizations/timeline-graph.component";
 import LoaderComponent from "../loader.component";
 import _ from "lodash";
 import moment from "moment";
-import DetailsAlertModal from "../../details.alert.modal.component"
+import DetailsAlertModal from "../../details.alert.modal.component";
 
 const AlertTimeLineGraph = (props) => {
   const { commonStore } = useContext(MobXProviderContext);
@@ -17,7 +15,7 @@ const AlertTimeLineGraph = (props) => {
   const [isLoading, setIsLoading] = useState(true);
   const [totalItems, setTotalItems] = useState(0);
   const [totalPages, setTotalPages] = useState(0);
-  const [selectedItem, setSelectedItem] = useState({})
+  const [selectedItem, setSelectedItem] = useState({});
   const [orderBy, setOrderBy] = useState(["created_at", "DESC"]);
   const [dateFilter, setDateFilter] = useState({
     from: null,
@@ -25,7 +23,7 @@ const AlertTimeLineGraph = (props) => {
   });
 
   const { type, id } = props;
-  
+
   useEffect(() => {
     setIsLoading(true);
     const alertPromise = commonStore.getData(
@@ -45,7 +43,7 @@ const AlertTimeLineGraph = (props) => {
     );
     Promise.all([alertPromise]).then((response) => {
       setAlerts(response[0].data.alerts);
-      setItems(getItems(response[0].data.alerts))
+      setItems(getItems(response[0].data.alerts));
       setTotalItems(response[0].data.total_items);
       setTotalPages(response[0].data.total_pages);
       setIsLoading(false);
@@ -54,24 +52,26 @@ const AlertTimeLineGraph = (props) => {
 
   const getItems = (data) => {
     return data.map((e, index) => {
-        return {
-          id: _.get(e, "id", index + 1),
-          content: _.truncate(_.get(e, "type.name", "N/A"), {length:16}),
-          allContent: e,
-          start: moment(e.created_at),
-          className: _.get(e, "type.risk", "").toLowerCase(),
-          group: _.get(e, "type.risk", "").toLowerCase()
-        };
-    }
-    )
-  }
+      return {
+        id: _.get(e, "id", index + 1),
+        content: _.truncate(_.get(e, "type.name", "N/A"), { length: 16 }),
+        allContent: e,
+        start: moment(e.created_at),
+        className: _.get(e, "type.risk", "").toLowerCase(),
+        group: _.get(e, "type.risk", "").toLowerCase(),
+      };
+    });
+  };
 
   const closeAlertDetails = () => {
-      setSelectedItem({})
-  }
+    setSelectedItem({});
+  };
   const clickItemEvent = (data) => {
-    setSelectedItem({ alert: _.get(data, "item.allContent", {}), alert_type: _.get(data, "item.allContent.type", {})})
-  }
+    setSelectedItem({
+      alert: _.get(data, "item.allContent", {}),
+      alert_type: _.get(data, "item.allContent.type", {}),
+    });
+  };
 
   // Configuration for the Timeline
   const options = {
@@ -80,23 +80,9 @@ const AlertTimeLineGraph = (props) => {
     minHeight: 350,
     editable: false,
     locale: "en",
-    /*template: function(item, element, data) {
-      return ReactDOM.render(
-        <Popup
-          basic
-          trigger={
-            <div className="ui error message">{_.get(item, "content", "")}</div>
-          }
-          content={_.get(item, "content", "")}
-        />,
-        element
-      );
-    },*/
-    //start: "2016-01-01",
-    //end: "2016-01-04",
     tooltip: {
-        template: function(originalItemData, parsedItemData) {
-          return `
+      template: function(originalItemData, parsedItemData) {
+        return `
             <div style="
             border: "1px solid #d4d4d5;
             line-height: "1.4285em";
@@ -120,50 +106,36 @@ const AlertTimeLineGraph = (props) => {
                   "allContent.type.name"
                 )}</small>
             </div>`;
-        }
-    }
-    /*min: new Date(2012, 0, 1), // lower limit of visible range
-      max: new Date(2013, 0, 1), // upper limit of visible range
-      zoomMin: 1000 * 60 * 60 * 24, // one day in milliseconds
-      zoomMax: 1000 * 60 * 60 * 24 * 31 * 3, // about three months in
-      */
-    /*},*/
+      },
+    },
   }
 
   var groups = [
     {
-      id: 'info',
+      id: "info",
       content: "INFO",
-      // Optional: a field 'className', 'style', 'order', [properties]
     },
     {
-      id: 'low',
+      id: "low",
       content: "LOW",
-      // Optional: a field 'className', 'style', 'order', [properties]
     },
     {
-      id: 'medium',
+      id: "medium",
       content: "MEDIUM",
-      // Optional: a field 'className', 'style', 'order', [properties]
     },
     {
-      id: 'high',
+      id: "high",
       content: "HIGH",
-      // Optional: a field 'className', 'style', 'order', [properties]
     },
-    // more groups...
-  ];
+  ]
 
   return (
     <React.Fragment>
       {_.isEmpty(items) && (
-        <LoaderComponent loadingMessage="Loading Alerts..." />
+        <LoaderComponent loadingMessage="Loading Alerts Timeline..." />
       )}
       {!_.isEmpty(selectedItem) && (
-        <DetailsAlertModal
-          alert={ selectedItem }
-          onClose={closeAlertDetails}
-        />
+        <DetailsAlertModal alert={selectedItem} onClose={closeAlertDetails} />
       )}
       {!_.isEmpty(items) && (
         <TimeLineGraph
@@ -175,7 +147,7 @@ const AlertTimeLineGraph = (props) => {
         ></TimeLineGraph>
       )}
     </React.Fragment>
-  );
-};
+  )
+}
 
 export default AlertTimeLineGraph;
