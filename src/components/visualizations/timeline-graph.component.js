5ac8166367c52351f3f81f6ab078dc1cb59370f7
Christian <c.pfarher@gmail.com>
2020-09-17 15:04:52 -0300

    delete some comments

--- src/components/visualizations/timeline-graph.component.js
+++ src/components/visualizations/timeline-graph.component.js
@@ -1,10 +1,7 @@
 import React, { useEffect, useState, useRef } from "react";
-import { Button, Grid, Segment, Popup } from "semantic-ui-react";
-import { MobXProviderContext } from "mobx-react";
-import moment from "moment";
+import { Button, Grid, Segment } from "semantic-ui-react";
 import _ from "lodash";
 import * as vis from "vis-timeline/standalone";
-import ReactDOM from "react-dom";
 
 const TimeLineGraph = (props) => {
   const defaultItems = [
@@ -28,7 +25,6 @@ const TimeLineGraph = (props) => {
 
   const {
     groups = [
-      // Configuration for the Timeline
       {
         id: "info",
         content: "INFO",
@@ -67,27 +63,15 @@ const TimeLineGraph = (props) => {
           element
         );
       },
-      //template: template, => handle bar template
-      //start: "2016-01-01",
-      //end: "2016-01-04",
-      /*tooltip: {
-                  template: function(originalItemData, parsedItemData) {
-                    var color = originalItemData.title == "IN_PROGRESS" ? "red" : "green";
-                    return `<span style="color:${color}">${originalItemData.title}</span>`;
-                  },*/
-      /*min: new Date(2012, 0, 1), // lower limit of visible range
-                max: new Date(2013, 0, 1), // upper limit of visible range
-                zoomMin: 1000 * 60 * 60 * 24, // one day in milliseconds
-                zoomMax: 1000 * 60 * 60 * 24 * 31 * 3, // about three months in
-                */
+     */
     },
   } = props;
 
   const showControlBar = props.showControlBar || false;
   const refElement = useRef(null);
-  const [items, setItems] = useState({})
+  const [items, setItems] = useState({});
   const [timeline, setTimeLine] = useState({});
-  
+
   const triggerClickItemEvent = (itemId) => {
     if (_.isFunction(props.onClickItemEvent)) {
       props.onClickItemEvent({
@@ -105,54 +89,26 @@ const TimeLineGraph = (props) => {
     );
   }, []);
 
-    useEffect(() => {
-      if (!_.isEmpty(timeline)) {
-        timeline.on("select", function(event) {
-          triggerClickItemEvent(_.get(event, "items[0]"))
-        });
-      }
-    }, [timeline]);
+  useEffect(() => {
+    if (!_.isEmpty(timeline)) {
+      timeline.on("select", function(event) {
+        triggerClickItemEvent(_.get(event, "items[0]"));
+      });
+    }
+  }, [timeline]);
 
   const fit = () => {
     timeline.fit();
- };
+  };
 
   const zoomOut = () => {
     timeline.zoomOut(1);
   };
-  
+
   const zoomIn = () => {
     timeline.zoomIn(1);
   };
-  /*
-      function stringifyObject(object) {
-        if (!object) return;
-        var replacer = function (key, value) {
-          if (value && value.tagName) {
-            return "DOM Element";
-          } else {
-            return value;
-          }
-        };
-        return JSON.stringify(object, replacer);
-      }
 
-      function logEvent(event, properties) {
-        var log = document.getElementById("log");
-        var msg = document.createElement("div");
-        msg.innerHTML =
-          "event=" +
-          JSON.stringify(event) +
-          ", " +
-          "properties=" +
-          stringifyObject(properties);
-        log.firstChild ? log.insertBefore(msg, log.firstChild) : log.appendChild(msg);
-      }
-      */
-  /*function setHoveredItem(id) {
-        var hoveredItem = document.getElementById("hoveredItem");
-        hoveredItem.innerHTML = "hoveredItem=" + id;
-      }*/
   return (
     <React.Fragment>
       <Segment>
