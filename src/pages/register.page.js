fb33afbfcc947e4240fffb5bcb8c714c4028b874
Miguel Storani <27182260+miguelstorani@users.noreply.github.com>
2020-09-25 16:09:47 -0300

    add back to login link in several pages (#77)

--- src/pages/register.page.js
+++ src/pages/register.page.js
@@ -2,14 +2,21 @@ import * as React from "react";
 import { Grid } from "semantic-ui-react";
 import "./login.page.css";
 import { observer, inject } from "mobx-react";
-import { Header, Segment, Form, Label, Button, Message } from "semantic-ui-react";
+import {
+  Header,
+  Segment,
+  Form,
+  Label,
+  Button,
+  Message,
+} from "semantic-ui-react";
 import Validation from "../util/validation";
 
 import RecaptchaComponent from "../components/utils/recaptcha.component";
 import PhoneComponent from "../components/utils/phone.component";
-import BackToLogin from "../components/utils/back-to-login.component"
+import BackToLogin from "../components/utils/back-to-login.component";
 
-import logo from '../img/rolaguard-logo-white.svg'
+import logo from "../img/rolaguard-logo-white.svg";
 
 @inject("usersStore", "messageStore", "authStore", "rolesStore")
 @observer
@@ -22,13 +29,13 @@ class RegisterPage extends React.Component {
         username: "",
         email: "",
         full_name: "",
-        phone: ""
+        phone: "",
       },
       formHasError: true,
       loading: false,
       showErrors: {},
       showSuccessMessage: false,
-      logoURL: logo
+      logoURL: logo,
     };
 
     this.resetErrors();
@@ -44,8 +51,8 @@ class RegisterPage extends React.Component {
         email: false,
         usernameHasWhiteSpaces: false,
         unknownError: false,
-        bad_recaptcha_token: false
-      }
+        bad_recaptcha_token: false,
+      },
     });
   }
 
@@ -60,10 +67,21 @@ class RegisterPage extends React.Component {
     user.username = user.username.trim();
 
     showErrors.usernameInvalid = !Validation.isValidUsername(user.username);
-    showErrors.fullNameTooLong = Validation.exceedsFullNameLength(user.full_name);
-    showErrors.emailTooLong = Validation.exceedsEmailLength(this.state.user.email);
+    showErrors.fullNameTooLong = Validation.exceedsFullNameLength(
+      user.full_name
+    );
+    showErrors.emailTooLong = Validation.exceedsEmailLength(
+      this.state.user.email
+    );
 
-    formHasError = formHasError || showErrors.username || showErrors.usernameInvalid || showErrors.fullNameTooLong || showErrors.emailTooLong ? true : false;
+    formHasError =
+      formHasError ||
+      showErrors.username ||
+      showErrors.usernameInvalid ||
+      showErrors.fullNameTooLong ||
+      showErrors.emailTooLong
+        ? true
+        : false;
 
     this.setState({ showErrors: showErrors, formHasError: formHasError });
     return formHasError;
@@ -86,7 +104,7 @@ class RegisterPage extends React.Component {
 
     this.setState({
       user: user,
-      showErrors: showErrors
+      showErrors: showErrors,
     });
   };
 
@@ -97,28 +115,39 @@ class RegisterPage extends React.Component {
     if (!errors) {
       this.setState({ loading: true });
 
-      this.props.usersStore.register(this.state.user,this.props.authStore.recaptcha_token).then((response) => {
-        this.setState({ loading: false });
+      this.props.usersStore
+        .register(this.state.user, this.props.authStore.recaptcha_token)
+        .then((response) => {
+          this.setState({ loading: false });
 
-        if (response.status !== 200) {
-          if (response.data.error === "User " + this.state.user.username + " already exists") {
-            showErrors.usernameExists = true;
-          } else if (response.data.error === "User " + this.state.user.username + " is not valid") {
-            showErrors.usernameInvalid = true;
-          } else if (response.data.error === "Email " + this.state.user.email + " is not available") {
-            showErrors.emailExists = true;
-          } else if (response.data.error === "Bad recaptcha token") {
-            showErrors.bad_recaptcha_token = true;
-            this.setState({ showErrors: showErrors, loading: false });
+          if (response.status !== 200) {
+            if (
+              response.data.error ===
+              "User " + this.state.user.username + " already exists"
+            ) {
+              showErrors.usernameExists = true;
+            } else if (
+              response.data.error ===
+              "User " + this.state.user.username + " is not valid"
+            ) {
+              showErrors.usernameInvalid = true;
+            } else if (
+              response.data.error ===
+              "Email " + this.state.user.email + " is not available"
+            ) {
+              showErrors.emailExists = true;
+            } else if (response.data.error === "Bad recaptcha token") {
+              showErrors.bad_recaptcha_token = true;
+              this.setState({ showErrors: showErrors, loading: false });
+            } else {
+              showErrors.unknownError = true;
+            }
+
+            this.setState({ showErrors: showErrors });
           } else {
-            showErrors.unknownError = true;
+            this.setState({ showSuccessMessage: true });
           }
-          
-          this.setState({ showErrors: showErrors });
-        } else {
-          this.setState({ showSuccessMessage: true });
-        }
-      });
+        });
     }
   };
 
@@ -126,129 +155,153 @@ class RegisterPage extends React.Component {
     const user = this.state.user;
     user.phone = phone;
 
-    this.setState({user: user});
-  }
+    this.setState({ user: user });
+  };
 
   render() {
     const { user, showSuccessMessage } = this.state;
-    let hideSave = Validation.isEmpty(user.full_name) ||
-          Validation.isEmpty(user.email) ||
-          Validation.isEmpty(user.username) ||
-          !this.props.authStore.recaptcha_token;
+    let hideSave =
+      Validation.isEmpty(user.full_name) ||
+      Validation.isEmpty(user.email) ||
+      Validation.isEmpty(user.username) ||
+      !this.props.authStore.recaptcha_token;
 
     return (
       <div className="login-form">
         <Grid
           textAlign="center"
           style={{ margin: 0, height: "100%" }}
-          verticalAlign="middle">
+          verticalAlign="middle"
+        >
           <Grid.Column className="animated fadeIn" style={{ maxWidth: 650 }}>
-        {!showSuccessMessage && ( 
-          <div>
-          <Header as="h2" inverted color="black" textAlign="center">
-            <img id="login-logo" className="animated fadeIn" src={this.state.logoURL} alt=""/>
-          </Header>
-          <Form noValidate="novalidate" className="form-label form-css-label" onSubmit={this.handleSubmit} style={{ marginTop: "2em" }} size="large"  >
-            <Segment textAlign="center">
-                <Form.Field required>
-                  <Form.Input
-                    disabled={this.state.typeForm === "Edit"}
-                    name="username"
-                    required
-                    value={user.username}
-                    onChange={this.handleChange}
-                    error={this.state.showErrors.username}
-                  >
-                    <input/>
-                    <label>Username</label>
-                  </Form.Input>
-                  {this.state.showErrors.usernameInvalid && (
-                    <Label className="mt0" basic color="red" pointing>
-                      Invalid username. Please enter at least 3 characters, a maximum of 32 characters, use only letters (a-z), numbers, periods or underscores. 
-                    </Label>
-                  )}
-                  {this.state.showErrors.usernameExists && (
-                    <Label className="mt0" basic color="red" pointing>
-                      We are sorry, that username already exists.
-                    </Label>
-                  )}
-                </Form.Field>
-
-                <Form.Field required>
-                  <Form.Input
-                    required
-                    name="full_name"
-                    value={user.full_name}
-                    onChange={this.handleChange}
-                    error={this.state.showErrors.full_name}
-                  >
-                    <input/>
-                    <label>Fullname</label>
-                  </Form.Input>
-                  {this.state.showErrors.fullNameTooLong && (
-                    <Label className="mt0" basic color="red" pointing>
-                      Full name is too long. Please enter a maximum of 64 characters.
-                    </Label>
-                  )}
-                </Form.Field>
-
-                <Form.Field required>
-                  <Form.Input
-                    required
-                    name="email"
-                    value={user.email}
-                    onChange={this.handleChange}
-                    disabled={this.state.typeForm === "Edit"}
-                    error={this.state.showErrors.email}
-                  >
-                    <input/>
-                    <label>E-Mail</label>
-                  </Form.Input>
-                  {this.state.showErrors.emailExists && (
-                    <Label className="mt0" basic color="red" pointing>
-                      We are sorry. That email already exists.
-                    </Label>
-                  )}
-                  {this.state.showErrors.emailTooLong && (
-                    <Label className="mt0" basic color="red" pointing>
-                      Email is too long. Please enter a maximum of 320 characters.
-                    </Label>
-                  )}
-                </Form.Field>
-                <Form.Field>
-                  <PhoneComponent onPhoneChange={this.onPhoneChange}></PhoneComponent>
-                </Form.Field>
-
-                <div className="recaptcha-component">
-                  <RecaptchaComponent />
-                </div>
-
-                <BackToLogin />
-
-                <Button 
-                  type="submit"
-                  color="blue"
+            {!showSuccessMessage && (
+              <div>
+                <Header as="h2" inverted color="black" textAlign="center">
+                  <img
+                    id="login-logo"
+                    className="animated fadeIn"
+                    src={this.state.logoURL}
+                    alt=""
+                  />
+                </Header>
+                <Form
+                  noValidate="novalidate"
+                  className="form-label form-css-label"
+                  onSubmit={this.handleSubmit}
+                  style={{ marginTop: "2em" }}
                   size="large"
-                  loading={this.state.loading}
-                  disabled={this.state.loading || hideSave}
-                  content="Register"
-                />
+                >
+                  <Segment textAlign="center">
+                    <Form.Field required>
+                      <Form.Input
+                        disabled={this.state.typeForm === "Edit"}
+                        name="username"
+                        required
+                        value={user.username}
+                        onChange={this.handleChange}
+                        error={this.state.showErrors.username}
+                      >
+                        <input />
+                        <label>Username</label>
+                      </Form.Input>
+                      {this.state.showErrors.usernameInvalid && (
+                        <Label className="mt0" basic color="red" pointing>
+                          Invalid username. Please enter at least 3 characters,
+                          a maximum of 32 characters, use only letters (a-z),
+                          numbers, periods or underscores.
+                        </Label>
+                      )}
+                      {this.state.showErrors.usernameExists && (
+                        <Label className="mt0" basic color="red" pointing>
+                          We are sorry, that username already exists.
+                        </Label>
+                      )}
+                    </Form.Field>
 
-            </Segment>
-          </Form>
-          </div> 
-          )}
+                    <Form.Field required>
+                      <Form.Input
+                        required
+                        name="full_name"
+                        value={user.full_name}
+                        onChange={this.handleChange}
+                        error={this.state.showErrors.full_name}
+                      >
+                        <input />
+                        <label>Fullname</label>
+                      </Form.Input>
+                      {this.state.showErrors.fullNameTooLong && (
+                        <Label className="mt0" basic color="red" pointing>
+                          Full name is too long. Please enter a maximum of 64
+                          characters.
+                        </Label>
+                      )}
+                    </Form.Field>
 
-          {showSuccessMessage && (
-            <Segment>
-              <Message className="mh-auto" color='green'>{'An email was sent to the account provided'}</Message>
-            </Segment>
-          )}
-          {this.state.showErrors.unknownError && (
-            <Segment>
-              <Message className="mh-auto" color='red'>We are sorry. It has been an error while trying to create your account.</Message>
-            </Segment>
-          )}
+                    <Form.Field required>
+                      <Form.Input
+                        required
+                        name="email"
+                        value={user.email}
+                        onChange={this.handleChange}
+                        disabled={this.state.typeForm === "Edit"}
+                        error={this.state.showErrors.email}
+                      >
+                        <input />
+                        <label>E-Mail</label>
+                      </Form.Input>
+                      {this.state.showErrors.emailExists && (
+                        <Label className="mt0" basic color="red" pointing>
+                          We are sorry. That email already exists.
+                        </Label>
+                      )}
+                      {this.state.showErrors.emailTooLong && (
+                        <Label className="mt0" basic color="red" pointing>
+                          Email is too long. Please enter a maximum of 320
+                          characters.
+                        </Label>
+                      )}
+                    </Form.Field>
+                    <Form.Field>
+                      <PhoneComponent
+                        onPhoneChange={this.onPhoneChange}
+                      ></PhoneComponent>
+                    </Form.Field>
+
+                    <div className="recaptcha-component">
+                      <RecaptchaComponent />
+                    </div>
+
+                    <BackToLogin />
+
+                    <Button
+                      type="submit"
+                      color="blue"
+                      size="large"
+                      loading={this.state.loading}
+                      disabled={this.state.loading || hideSave}
+                      content="Register"
+                    />
+                  </Segment>
+                </Form>
+              </div>
+            )}
+
+            {showSuccessMessage && (
+              <Segment>
+                <Message className="mh-auto" color="green">
+                  An email was sent to the account provided.{" "}
+                  <a href="/login">back to login</a>
+                </Message>
+              </Segment>
+            )}
+            {this.state.showErrors.unknownError && (
+              <Segment>
+                <Message className="mh-auto" color="red">
+                  We are sorry. It has been an error while trying to create your
+                  account.
+                </Message>
+              </Segment>
+            )}
           </Grid.Column>
         </Grid>
       </div>
