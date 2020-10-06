2b78d1b243e28b41dd291138d36210f9f4e3a2be
Christian <c.pfarher@gmail.com>
2020-10-06 10:52:07 -0300

    convert to functional component

--- src/pages/notFoundPage.page.js
+++ src/pages/notFoundPage.page.js
@@ -1,34 +1,25 @@
-import * as React from 'react';
-import { observer, inject } from "mobx-react";
+import * as React from "react";
 import "./notFoundPage.page.css";
-import {Icon} from "semantic-ui-react";
+import { Icon } from "semantic-ui-react";
+import { withRouter } from "react-router-dom";
 
-@inject()
-@observer
-class NotFoundPage extends React.Component {
+const NotFoundPage = (props) => {
 
-    constructor(props){
-        super(props)
-    }
+  const {status, statusText, history} = props
+  
+  return (
+    <div className="not-found-page">
+      <div className="animated fadeIn not-found-text">
+        <Icon name="exclamation" />
+        <h1>{status}</h1>
+      </div>
+      <h5 className="animated fadeIn">{statusText}</h5>
 
-    render() {
-        return (
-            <div className="not-found-page">
-                <div className="animated fadeIn not-found-text">
-                    <Icon name="exclamation"/>
-                    <h1>404</h1>
-                </div>
-                <h5 className="animated fadeIn">Page not found</h5>
+      <button className="animated fadeIn" style={{cursor: "pointer"}} onClick={() => {history.push('/dashboard')}}>
+        GO BACK
+      </button>
+    </div>
+  );
+};
 
-                <button className="animated fadeIn" onClick={this.returnDashboard}>GO BACK</button>
-            </div>
-        )
-    }
-
-    returnDashboard = () => {
-        this.props.history.push("/dashboard");
-    }
-
-}
-
-export default NotFoundPage
+export default withRouter(NotFoundPage);
