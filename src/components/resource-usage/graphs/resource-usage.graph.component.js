c2a209d62a4599ab4fb786c7e53dd44c3fcc9678
Christian <c.pfarher@gmail.com>
2020-10-23 18:22:50 -0300

    add data source filter component and add to network overview

--- src/components/resource-usage/graphs/resource-usage.graph.component.js
+++ src/components/resource-usage/graphs/resource-usage.graph.component.js
@@ -1,16 +1,26 @@
 import React, { } from "react";
+import { MobXProviderContext, observer } from "mobx-react";
 import { Segment, Grid } from "semantic-ui-react";
 import ResourceUsageGraphStatusComponent from "./resource-usage.graph.status.component";
 import ResourceUsageGraphGatewaysComponent from "./resource-usage.graph.gateways.component";
 import ResourceUsageGraphPacketsLostComponent from "./resource-usage.graph.packets-lost.component";
 import ResourceUsageGraphSignalStrengthComponent from "./resource-usage.graph.signal-strength.component";
+import DataCollectorSelector from "../../utils/data-collector-selector.component";
 
 const ResourceUsageGraphComponent = (props) => {
   //const storeDummyData=getDummyDataForGraphs();
+  const { resourceUsageStore } = React.useContext(MobXProviderContext);
+
+  const handleChangeDataCollector = (params) => {
+    resourceUsageStore.setCriteria({ data_collectors: params.selected });
+  }
 
   return (
     <Segment>
       <Grid className="animated fadeIn">
+        <Grid.Row className="data-container pl pr">
+          <DataCollectorSelector onChange={handleChangeDataCollector} />
+        </Grid.Row>
         <Grid.Row id="visualization-container" className="data-container pl pr">
           <Grid.Column
             className="data-container-box pl0 pr0"
